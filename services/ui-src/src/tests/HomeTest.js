dbe36ba8282912d6b64c3d91d4b723a4e03a2749
Andrew Adcock <andrewadcock@gmail.com>
2021-01-08 16:20:01 -0500

    Setup nightwatch and initial test

---
+++ services/ui-src/src/tests/HomeTest.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'Test 1 Go to google' : function (client) {
+        client
+            .url('https://www.google.com')
+            .assert.visible('body')
+            .end();
+    }
+}
