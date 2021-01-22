e77a41af9b0bf8c08a9bdb87fe8e74f0d316e411
Alexis <awoodbury@collabralink.com>
2021-01-22 17:13:58 -0500

    Shell complete, considering iterable variable for tab contents

---
+++ services/ui-src/src/components/layout/TabContainer.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+import {
+  Accordion,
+  Alert,
+  Button,
+  Grid,
+  GridContainer
+} from "@trussworks/react-uswds";
+
+const TabContainer = () => {
+  let tabName;
+  return (
+    <GridContainer className="tab-container">
+      <Grid row>
+        <Grid col={12}>
+          <Tabs>
+            <TabList>
+              {fakeTabData.map((tab, idx) => {
+                const tabName =
+                  tab.age_range === "Under 0"
+                    ? "Under Age 0"
+                    : `Ages ${tab.age_range}`;
+                return <Tab key={idx}>{tabName}</Tab>;
+              })}
+              <Tab>Summary</Tab>
+              <Tab>Certification</Tab>
+            </TabList>
+          </Tabs>
+        </Grid>
+      </Grid>
+    </GridContainer>
+  );
+};
+
+const fakeTabData = [
+  {
+    age_range: "Under 0",
+    age_description: "Conception to birth"
+  },
+  {
+    age_range: "0 - 1",
+    age_description: "Birth through age 12 months"
+  },
+  {
+    age_range: "1 - 5",
+    age_description: "Age 1 year through age 5 years"
+  },
+  {
+    age_range: "6 - 12",
+    age_description: "Age 6 years through age 12 years"
+  },
+  {
+    age_range: "13 - 18",
+    age_description: "Age 13 years through age 18 years"
+  }
+];
+
+export default TabContainer;
