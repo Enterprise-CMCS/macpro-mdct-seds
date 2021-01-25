e9822431a0fb06e40d26ddaa6978f0e4d01a5312
Alexis <awoodbury@collabralink.com>
2021-01-25 10:30:04 -0500

    added comments for queston component placement

--- services/ui-src/src/components/layout/TabContainer.js
+++ services/ui-src/src/components/layout/TabContainer.js
@@ -20,23 +20,16 @@ const TabContainer = () => {
           <Tabs>
             <TabList>
               {fakeTabData.map((tab, idx) => {
-                const tabName =
-                  tab.age_range === "Under 0"
-                    ? "Under Age 0"
-                    : `Ages ${tab.age_range}`;
-                return <Tab key={idx}>{tabName}</Tab>;
+                return <Tab key={idx}>{tab.age_range}</Tab>;
               })}
               <Tab>Summary</Tab>
               <Tab>Certification</Tab>
             </TabList>
 
-            {
-              fakeTabData.map((tab, idx) => {
-                return <TabPanel key={idx}>{tab.age_description}</TabPanel>;
-              })
-              // The actual questions will go inside of the tab panels, find a way to componenetize that
-              // What will the contents of the Summary and Certification tabs be?
-            }
+            {fakeTabData.map((tab, idx) => {
+              // The questions for each tab will go inside of the tab Panels
+              return <TabPanel key={idx}>{tab.age_description}</TabPanel>;
+            })}
 
             <TabPanel>Summary </TabPanel>
             <TabPanel>Certification</TabPanel>
@@ -46,26 +39,27 @@ const TabContainer = () => {
     </GridContainer>
   );
 };
+// The actual questions will go inside of the tab panels, find a way to componenetize that
 
 const fakeTabData = [
   {
-    age_range: "Under 0",
+    age_range: "Under Age 0",
     age_description: "Conception to birth"
   },
   {
-    age_range: "0 - 1",
+    age_range: "Ages 0 - 1",
     age_description: "Birth through age 12 months"
   },
   {
-    age_range: "1 - 5",
+    age_range: "Ages 1 - 5",
     age_description: "Age 1 year through age 5 years"
   },
   {
-    age_range: "6 - 12",
+    age_range: "Ages 6 - 12",
     age_description: "Age 6 years through age 12 years"
   },
   {
-    age_range: "13 - 18",
+    age_range: "Ages 13 - 18",
     age_description: "Age 13 years through age 18 years"
   }
 ];
