5688e8c3f58bccdc4848527e16ebe2ff410c3247
Alexis <awoodbury@collabralink.com>
2021-01-28 13:27:52 -0500

    waypoint, status button under construction

--- services/ui-src/src/containers/NotFound.js
+++ services/ui-src/src/containers/NotFound.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./NotFound.css";
 
-export default function NotFound() {
+import { disableForm } from "../store/reducers/singleForm";
+import { Button } from "@trussworks/react-uswds";
+
+const NotFound = ({ form }) => {
+  const toggleStatus = form.status === "active" ? "N/A" : "active";
   return (
     <div className="NotFound">
+      <Button onClick={disableForm(toggleStatus)}>{form.status}</Button>
       <h3>Sorry, page not found!</h3>
     </div>
   );
-}
+};
+
+const mapState = state => ({
+  form: state.currentForm
+});
+
+export default connect(mapState)(NotFound);
--- services/ui-src/src/store/reducers/singleForm.js
+++ services/ui-src/src/store/reducers/singleForm.js
@@ -3,6 +3,7 @@ import * as data from "../toDelete/21E.json";
 
 // ACTION TYPES
 export const LOAD_SINGLE_FORM = "LOAD_SINGLE_FORM";
+export const UPDATE_FORM_STATUS = "UPDATE_FORM_STATUS";
 
 // ACTION CREATORS
 export const gotFormData = (formArray = {}) => {
@@ -11,21 +12,34 @@ export const gotFormData = (formArray = {}) => {
     formArray
   };
 };
+export const updatedStatus = (updateObj = {}) => ({
+  type: UPDATE_FORM_STATUS,
+  activeStatus: updateObj
+});
 
 // THUNKS
-// Make call to aws-amplify // WEB SOCKETS?
+// Make call to aws-amplify
 export const getFormData = ({ formID }) => {
   return async dispatch => {
     // Call aws amplify endpoint. This is a placeholder
     // const { data } = results;
+    // will also have to fetch status of form, this is currently not included
 
     dispatch(gotFormData(data));
+  };
+};
+export const disableForm = (dispatch, activeStatus) => {
+  return async dispatch => {
+    // Call aws amplify endpoint to update the status of this form.
+    // const { data } = results;
+    dispatch(updatedStatus({ status: activeStatus }));
   };
 };
 
 // INITIAL STATE
 const initialState = {
-  currentForm: [...data.default]
+  currentForm: [...data.default],
+  status: "active"
 };
 
 // REDUCER
@@ -35,6 +49,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         currentForm: action.formArray
+      };
+    case UPDATE_FORM_STATUS:
+      return {
+        ...state,
+        status: action.activeStatus.status
       };
     default:
       return state;
