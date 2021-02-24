24511c65ad6b8cf61d48519711699d4b787a05b6
Alexis <awoodbury@collabralink.com>
2021-02-23 20:10:39 -0500

    component planned in full

--- services/ui-src/src/components/FormPage.js
+++ services/ui-src/src/components/FormPage.js
@@ -5,3 +5,27 @@ const FormPage = () => {
 };
 
 export default FormPage;
+
+// TODO:
+// copy base of page from examples.js
+// inspect redux, is most of what is needed present
+// connect to redux
+// add subheader bar with links to the top of the page with quarter & home
+// add form name to subheader bar
+// make div for tab container to go in
+// add tab container
+// add button bar at the bottom (back, save, submit)
+// just outside of the tab container area should be the heading "Number of CHIP Children Served, Separate Child Health Program"
+// just outside of the tab container area should be the form description
+// just outside of the tab container area should be program code, state and quarter
+// make a question component that houses the table component- WITH question text
+// consider where "isActive" property should be read to disable the entire page
+
+// REDUX: does the App provider need to change locations to supply this page with all that it needs?
+
+// LIST OF MISSING/DUMMY DATA:
+// These things are currently being populated with dummy data but will need to be implemented eventually
+
+// ASKS:
+// Where is the "does not apply" meant to be put? it would be nice to remove it from the not found page
+// Is there a ticket for implementing the summary tab functionality?
