c45b9a17026a63f909a4022e8abe2d2608f8a072
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-02-25 14:06:57 -0500

    Convert ismemberof to role for saving

--- services/ui-src/src/containers/LocalLogin.js
+++ services/ui-src/src/containers/LocalLogin.js
@@ -14,8 +14,8 @@ export default function Login() {
       firstName: "Alice",
       lastName: "Cooper",
       lastLogin: "01/28/2021",
-      role: "state",
-      // role:
+      ismemberof: "state",
+      // ismemberof:
       //   "cn=CARTS_Group_Dev,ou=Groups,dc=cms,dc=hhs,dc=gov,cn=CARTS_Group_Dev_Admin,ou=Groups,dc=cms,dc=hhs,dc=gov,cn=CHIP_D_USER_GROUP_ADMIN,ou=Groups,dc=cms,dc=hhs,dc=gov,cn=IDM_OKTA_TEST,ou=Groups,dc=cms,dc=hhs,dc=gov",
       dateJoined: "01/22/2021",
       isSuperUser: "true",
