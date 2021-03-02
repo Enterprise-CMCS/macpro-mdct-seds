8a1fc0d49682be7e0b138224dc08a508182a9224
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-03-02 09:28:22 -0500

    Add loop for creating JSON

--- services/ui-src/src/containers/HomeState.js
+++ services/ui-src/src/containers/HomeState.js
@@ -2,34 +2,60 @@ import React from "react";
 import { Accordion, Grid, Link } from "@trussworks/react-uswds";
 import { connect } from "react-redux";
 
+const HomeState = ({ userData }) => {
+  // Pull state from redux
+  const state = userData.abbrev;
+  // TODO: Pull years and quarters
+  // define years and months
+  const date = new Date();
+  let fiscalYear = date.getFullYear();
+  console.log(fiscalYear);
+  let month = date.getMonth() + 1;
+  // create array years with subarrays year and quarters
+  // const years = [...year, ...quarters];
+  let years;
+  const year = [];
+  const quarters = [];
 
-const HomeState = ({userData}) => {
-  // TODO: Pull state from redux
-<<<<<<< HEAD
-  const state = userData.abbrev || "AK";
-=======
-  const state = "AL";
->>>>>>> 01079caba0536b59764d632e78f6f363d9604034
+  // for loops to populate subarrays
+  function dateMachine(fiscalYear, month) {
+    let year = fiscalYear;
+    let jsonData = [];
+    console.log("fiscalYear", fiscalYear);
+    for (let i = fiscalYear; i >= 2018; i--) {
+      console.log("fiscalYear");
+      let currentYear;
+      let currentQuarters;
 
-  // TODO: Pull years and quarters from redux
-  const years = [
-    {
-      year: 2021,
-      quarters: ["1", "2"]
-    },
-    {
-      year: 2020,
-      quarters: ["1", "2", "3", "4"]
-    },
-    {
-      year: 2019,
-      quarters: ["1", "2", "3", "4"]
-    },
-    {
-      year: 2018,
-      quarters: ["1", "2", "3", "4"]
+      if (i === fiscalYear) {
+        currentYear = fiscalYear;
+        if (month > 9) {
+          currentYear = fiscalYear + 1;
+          currentQuarters = [1];
+        } else if (month < 4) {
+          currentQuarters = [1, 2];
+        } else if (month > 3 && month < 7) {
+          currentQuarters = [1, 2, 3];
+        } else {
+          currentQuarters = [1, 2, 3, 4];
+        }
+
+        jsonData.push({
+          year: currentYear,
+          quarters: currentQuarters
+        });
+      } else {
+        jsonData.push({
+          year: i,
+          quarters: [1, 2, 3, 4]
+        });
+      }
     }
-  ];
+    return jsonData;
+  }
+
+  const test = dateMachine(2021, 6);
+  console.log("dateMachine output", test);
 
   let accordionItems = [];
   for (const year in years) {
@@ -39,7 +65,7 @@ const HomeState = ({userData}) => {
         {years[year].quarters.map(element => {
           return (
             <li key={`${state}-${year}-${element}`}>
-              <Link href={`/forms/${state}/${years[year].year}/${element}`}>
+              <Link href={`/forms/${state}/${year}/${element}`}>
                 Quarter {element}
               </Link>
             </li>
@@ -47,25 +73,21 @@ const HomeState = ({userData}) => {
         })}
       </ul>
     );
-
     // If current year, set expanded to true
     let expanded = false;
-    const date = new Date();
-    if (years[year].year === date.getFullYear()) {
+    if (year === date.getFullYear()) {
       expanded = true;
     }
-
     // Build single item
     let item = {
       id: year,
-      description: "Quarters for " + years[year].year,
-      title: years[year].year,
+      description: "Quarters for " + year,
+      title: year,
       content: quarters,
       expanded: expanded
     };
     accordionItems.push(item);
   }
-
   return (
     <Grid row className="page-home-state">
       <Grid col={12}>
@@ -80,8 +102,7 @@ const HomeState = ({userData}) => {
     </Grid>
   );
 };
-
 const mapState = state => ({
   userData: state.userData.userState
 });
-export default connect(mapstate)(HomeState);
+export default connect(mapState)(HomeState);
