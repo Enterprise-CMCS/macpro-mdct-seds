da027ea1ad42d3c757e4b22b7efdde1b803a314f
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2021-03-03 12:57:36 -0500

    Revert "Update users to have better filenames/paths"

--- services/app-api/handlers/users/create.js
+++ services/app-api/handlers/users/create.js
@@ -1,6 +1,6 @@
-import handler from "../../../libs/handler-lib";
-import dynamoDb from "../../../libs/dynamodb-lib";
-import { main as getUserByUsername } from "./getUserByUsername";
+import handler from "./../../libs/handler-lib";
+import dynamoDb from "./../../libs/dynamodb-lib";
+import { getUserByUsername } from "./get";
 
 export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
