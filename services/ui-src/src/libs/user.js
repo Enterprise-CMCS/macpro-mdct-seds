2690ed9fe37742c49baf6bc1f6055d739706faeb
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-03-04 10:33:28 -0500

    Fix minor bug: missing curly brace

--- services/ui-src/src/libs/user.js
+++ services/ui-src/src/libs/user.js
@@ -6,7 +6,7 @@ const userKey = "userKey";
 export async function currentUserInfo() {
   // Temporary change for local login
   // if (config.LOCAL_LOGIN === "true") {
-  if(true)
+  if (true) {
     return getLocalUserInfo();
   } else {
     // return Auth.currentUserInfo();
