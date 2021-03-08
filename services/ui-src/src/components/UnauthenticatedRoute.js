87f7209834df51be31e14cd56fdbdd5fc3490f0f
eolaniyan052 <eolaniyan@collabralink.com>
2021-03-08 14:37:09 -0500

    remove UnauthenticatedRoute

--- services/ui-src/src/components/UnauthenticatedRoute.js
+++ services/ui-src/src/components/UnauthenticatedRoute.js
@@ -20,15 +20,5 @@ function querystring(name, url = window.location.href) {
 }
 
 export default function UnauthenticatedRoute({ children, ...rest }) {
-  const { isAuthenticated } = useAppContext();
-  const redirect = querystring("redirect");
-  return (
-    <Route {...rest}>
-      {!isAuthenticated ? (
-        children
-      ) : (
-        <Redirect to={redirect === "" || redirect === null ? "/" : redirect} />
-      )}
-    </Route>
-  );
+  return <Route {...rest}>{children}</Route>;
 }
