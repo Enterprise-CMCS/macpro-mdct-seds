8db8e3eeb424b4a7738ad81a0103957635912e60
TGriesemer <TGriesemer@collabralink.com>
2021-03-08 09:54:07 -0500

    eslint on api functions

--- services/app-api/handlers/users/post/createUser.js
+++ services/app-api/handlers/users/post/createUser.js
@@ -2,7 +2,7 @@ import handler from "../../../libs/handler-lib";
 import dynamoDb from "../../../libs/dynamodb-lib";
 import { main as getUserByUsername } from "./getUserByUsername";
 
-export const main = handler(async (event, context) => {
+export const main = handler(async (event) => {
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
