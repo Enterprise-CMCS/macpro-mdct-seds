725637f49b0d3e8a1cadb9a36c5e474bea904872
cl-adavyd <tdavydets@collabralink.com>
2021-03-22 18:00:57 -0400

    api name changed to mdct-seds from amendments

--- services/ui-src/src/libs/api.js
+++ services/ui-src/src/libs/api.js
@@ -1,68 +1,90 @@
 import { API } from "aws-amplify";
 
+/*************************** HELPER FUNCTIONS ***************************/
 const requestOptions = () => {
   return {};
 };
 
+/*************************** USER API ***************************/
+// *** export to excel
 export const exportToExcel = async () => {
   const opts = requestOptions();
 
   return API.post("mdct-seds", "/users/export-to-excel", opts);
 };
 
-export function listUsers() {
+// *** list all users
+export const listUsers = () => {
   const opts = requestOptions();
+
   return API.get("mdct-seds", `/users`, opts);
-}
+};
 
-export function activateDeactivateUser(data) {
+// *** activate / deactivate user
+export const activateDeactivateUser = data => {
   const opts = requestOptions();
   opts.body = data;
+
   return API.post("mdct-seds", `/users/activation/${data.username}`, opts);
-}
+};
 
-export function getUserById(data) {
+// *** get user information by user id
+export const getUserById = data => {
   const opts = requestOptions();
+
   return API.get("mdct-seds", `/users/${data.userId}`, opts);
-}
+};
 
-export function getUserByUsername(data) {
+// *** get user information by username
+export const getUserByUsername = data => {
   const opts = requestOptions();
   opts.body = data;
+
   return API.post("mdct-seds", `/users/get`, opts);
-}
+};
 
-export function updateUser(data) {
+// *** update user information
+export const updateUser = data => {
   const opts = requestOptions();
   opts.body = data;
+
   return API.post("mdct-seds", `/users/update/${data.userId}`, opts);
-}
+};
 
-export function createUser(data) {
+// *** create user
+export const createUser = data => {
   const opts = requestOptions();
   opts.body = data;
-  return API.post("mdct-seds", `/users/add`, opts);
-}
 
-export function getStateForms(stateId, specifiedYear, quarter) {
+  return API.post("mdct-seds", `/users/add`, opts);
+};
+
+/*************************** FORMS API ***************************/
+// *** get forms associated with a specified state for specified year and quarter
+export const getStateForms = (stateId, specifiedYear, quarter) => {
   const opts = requestOptions();
+
   return API.get(
     "mdct-seds",
     `/forms/${stateId}/${specifiedYear}/${quarter}`,
     opts
   );
-}
+};
 
-export function getSingleForm(state, specifiedYear, quarter, form) {
+// *** get single form associated with a specified state, year and quarter
+export const getSingleForm = (state, specifiedYear, quarter, form) => {
   const opts = requestOptions();
+
   return API.get(
     "mdct-seds",
     `/single-form/${state}/${specifiedYear}/${quarter}/${form}`,
     opts
   );
-}
+};
 
-export function getFormTypes() {
+// *** get form types
+export const getFormTypes = _ => {
   const opts = requestOptions();
+
   return API.get("mdct-seds", "/form-types", opts);
-}
+};
