e22ffbb0a8b9ff46b080a30aaf2e2770f6e1207c
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-04-05 13:30:01 -0400

    Add tests for HomeAdmin

--- services/ui-src/src/components/HomeAdmin/HomeAdmin.js
+++ services/ui-src/src/components/HomeAdmin/HomeAdmin.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link } from "@trussworks/react-uswds";
 
 const HomeAdmin = () => {
   return (
@@ -8,13 +8,13 @@ const HomeAdmin = () => {
       <h1 className="page-header">Home Admin User Page</h1>
       <div className="padding-left-9 margin-left-9 list-display-container">
         <ul>
-          <li>
-            <Link to="/users" className="text-bold">
+          <li className="user-view-edit">
+            <Link href="/users" className="text-bold">
               View / Edit Users
             </Link>
           </li>
-          <li>
-            <Link to="/users/add" className="text-bold">
+          <li className="user-add">
+            <Link href="/users/add" className="text-bold">
               Create User
             </Link>
           </li>
