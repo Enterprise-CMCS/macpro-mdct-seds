d442cfc4b0e7297e30a46524077c83a466b9db26
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2021-04-05 09:26:21 -0400

    start test build; put FormFooter in div for testing purposes

--- services/ui-src/src/components/FormFooter/FormFooter.js
+++ services/ui-src/src/components/FormFooter/FormFooter.js
@@ -10,27 +10,29 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 const FormFooter = ({ state, year, quarter, lastModified }) => {
   const quarterPath = `/forms/${state}/${year}/${quarter}`;
   return (
-    <GridContainer>
-      <Grid row>
-        <Grid col={6} className="form-nav">
-          <Link to={quarterPath}>
-            {" "}
-            <FontAwesomeIcon icon={faArrowLeft} /> Back to{" "}
-            {`Q${quarter} ${year}`}
-          </Link>
-        </Grid>
-
-        <Grid col={6} className="form-actions">
-          <Grid row>
-            <Grid col={6}> Last saved: {lastModified} </Grid>
-            <Grid col={6}>
+    <div>
+      <GridContainer>
+        <Grid row>
+          <Grid col={6} className="form-nav">
+            <Link to={quarterPath}>
               {" "}
-              <Button className="hollow">Save</Button>
+              <FontAwesomeIcon icon={faArrowLeft} /> Back to{" "}
+              {`Q${quarter} ${year}`}
+            </Link>
+          </Grid>
+
+          <Grid col={6} className="form-actions">
+            <Grid row>
+              <Grid col={6}> Last saved: {lastModified} </Grid>
+              <Grid col={6}>
+                {" "}
+                <Button className="hollow">Save</Button>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </GridContainer>
+      </GridContainer>
+    </div>
   );
 };
 
---
+++ services/ui-src/src/components/FormFooter/FormFooter.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import FormFooter from "./FormFooter";
+import { render } from "@testing-library/react";
+
+let realUseContext;
+let useContextMock;
+
+// *** set up mocks
+beforeEach(() => {
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
+});
+
+// *** garbage clean up (mocks)
+afterEach(() => {
+  React.useContext = realUseContext;
+});
+
+describe("Test FormFooter.js", () => {
+  test("Check the main element, with classname footer, exists", () => {
+    useContextMock.mockReturnValue(true);
+
+    const mockUser = { attributes: { "app-role": "admin" } };
+
+    const { getByTestId } = render(<FormFooter user={mockUser} />);
+
+    expect(getByTestId("Footer")).toBeVisible();
+  });
+
+  test("Check for CMS Logo", () => {
+    const { getByAltText } = render(<Footer />);
+    const image = getByAltText("Centers for Medicare and Medicaid Services");
+    expect(image.src).toContain("/img/logo-cms.png");
+  });
+
+  test("Check for MDCT Logo", () => {
+    const { getByAltText } = render(<Footer />);
+    const image = getByAltText("Medicaid & CHIP Program System");
+    expect(image.src).toContain("/img/logo-mdct.png");
+  });
+
+  test("Check for Contact link", () => {
+    const { getByText } = render(<Footer />);
+    expect(getByText("Contact")).toHaveAttribute("href", "/contact");
+  });
+
+  test("Check for CMS Home Page link", () => {
+    const { getByText } = render(<Footer />);
+    expect(
+      getByText("Centers for Medicare & Medicaid Services Website")
+    ).toHaveAttribute("href", "https://www.cms.gov/");
+  });
+
+  test("Check for SEDS Help Desk email link", () => {
+    const { getByText } = render(<Footer />);
+    expect(getByText("SEDSHELP@cms.hhs.gov")).toHaveAttribute(
+      "href",
+      "mailto:sedshelp@cms.hhs.gov"
+    );
+  });
+});
