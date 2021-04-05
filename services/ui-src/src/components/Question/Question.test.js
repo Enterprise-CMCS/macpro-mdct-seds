5d7485a8c324b644992c5fd5f88ed9b6b9283f6b
TGriesemer <TGriesemer@collabralink.com>
2021-04-05 16:20:57 -0400

    functioning jest tests for Question.js

--- services/ui-src/src/components/Question/Question.test.js
+++ services/ui-src/src/components/Question/Question.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
-import TabContainer from "../layout/TabContainer";
+import TabContainer from "../TabContainer/TabContainer.js";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import currentFormMock_64_21E from "../../providerMocks/currentFormMock_64_21E.js";
@@ -13,9 +13,27 @@ describe("Test Question.js", () => {
   beforeEach(() => {
     store = mockStore(currentFormMock_64_21E);
     wrapper = mount(
-      <Provider store={store}>
-        <TabContainer />
-      </Provider>
+        <Provider store={store}>
+          <TabContainer />
+        </Provider>
     );
+  });
+
+  test("Check for all question numbers and the beginning of the question", () => {
+    expect(wrapper.text()).toMatch(/1. What is the/);
+    expect(wrapper.text()).toMatch(/2. What is the/);
+    expect(wrapper.text()).toMatch(/3. What is the/);
+    expect(wrapper.text()).toMatch(/4. What is the/);
+    expect(wrapper.text()).toMatch(/5. What is the/);
+    expect(wrapper.text()).toMatch(/6. What is the/);
+
+
+  });
+
+  test("Check for correct age range", () => {
+    expect(wrapper.text()).toMatch(/Under Age 0/);
+  });
+  test("Check number of gridwithtotal elements", () => {
+    expect(wrapper.find(".grid-with-totals").length).toBe(6);
   });
 });
