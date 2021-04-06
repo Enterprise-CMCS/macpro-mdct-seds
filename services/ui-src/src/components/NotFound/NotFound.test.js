73e1af5c88fd8c3b2dd066a078c3977120a39ae4
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2021-04-06 07:57:18 -0400

    run prettier

--- services/ui-src/src/components/NotFound/NotFound.test.js
+++ services/ui-src/src/components/NotFound/NotFound.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {mount} from "enzyme";
+import { mount } from "enzyme";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 
@@ -9,19 +9,19 @@ import currentFormMock_21E from "../../provider-mocks/currentFormMock_21E.js";
 const mockStore = configureStore([]);
 
 describe("Test NotFound.js", () => {
-    let store;
-    let wrapper;
+  let store;
+  let wrapper;
 
-    beforeEach(() => {
-        store = mockStore(currentFormMock_21E);
-        wrapper = mount(
-            <Provider store={store}>
-                <NotFound />
-            </Provider>
-        );
-    });
+  beforeEach(() => {
+    store = mockStore(currentFormMock_21E);
+    wrapper = mount(
+      <Provider store={store}>
+        <NotFound />
+      </Provider>
+    );
+  });
 
-    test("Check the main div, with classname NotFound exists", () => {
-        expect(wrapper.find(".NotFound").length).toBe(1);
-    });
+  test("Check the main div, with classname NotFound exists", () => {
+    expect(wrapper.find(".NotFound").length).toBe(1);
+  });
 });
