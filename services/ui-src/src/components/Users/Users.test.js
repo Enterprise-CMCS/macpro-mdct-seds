4e0ff3759ee4ee96977144c6cc28368f077f6765
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-06 14:40:20 -0400

    users test

---
+++ services/ui-src/src/components/Users/Users.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Users from "./Users";
+import { render } from "@testing-library/react";
+
+let realUseContext;
+let useContextMock;
+
+// *** set up mocks
+beforeEach(() => {
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
+});
+
+// *** garbage clean up (mocks)
+afterEach(() => {
+  React.useContext = realUseContext;
+});
+
+describe("Test Header.js", () => {
+  test("Check the main element, with classname user-profiles, exists", () => {
+    useContextMock.mockReturnValue(true);
+    const mockUser = { attributes: { "app-role": "admin" } };
+    // const { getByTestId } = render(<Users />);
+    // expect(getByTestId("Users")).toBeVisible();
+  });
+});
