92f874ade7b5f1968962558ddb2f9dd0f00ad21e
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-04-07 09:28:27 -0400

    Add test for authenticated route

---
+++ services/ui-src/src/components/AuthenticatedRoute/AuthenticatedRoute.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { mount } from "enzyme";
+import AuthenticatedRoute from "./AuthenticatedRoute";
+import * as AppContext from "../../libs/contextLib";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Mock for useLocation
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "localhost:3000/"
+  })
+}));
+
+describe("AuthenticatedRoute tests", () => {
+  test("Check the main element, with classname LoaderButton, exists", () => {
+    // Set Context values
+    const contextValues = { isAuthenticated: true };
+
+    // Mock useAppContext with new values
+    jest
+      .spyOn(AppContext, "useAppContext")
+      .mockImplementation(() => contextValues);
+
+    const wrapper = mount(
+      <BrowserRouter>
+        <AuthenticatedRoute />
+      </BrowserRouter>
+    );
+
+    expect(wrapper.containsMatchingElement(<Route />)).toEqual(true);
+  });
+});
