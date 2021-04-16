20d095e7a286a1fc1cf12e6397c9d3e318a9b5eb
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-16 18:24:14 -0400

    uncertified email handler logic

--- services/app-api/handlers/notification/uncertified.js
+++ services/app-api/handlers/notification/uncertified.js
@@ -1,8 +1,10 @@
 import handler from "../../libs/handler-lib"
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-east-1" });
 
 /**
- * Handler responsible for sending notification to bussiness users
- * that have not submitted their data at the end of a quarter - all reports in progress
+ * Handler responsible for sending notification to business users,
+ * each time a state takes an uncertify action on any of their quarterly forms
  */
 
 export const main = handler (async (event, context) => {
@@ -11,5 +13,52 @@ export const main = handler (async (event, context) => {
     console.log("Warmed up!");
     return null;
   }
+  let data = JSON.parse(event.body);
+  const email = unCetifiedTemplate(data)
+
+  console.log(email)
+
+  ses.sendEmail(email, function (err, data) {
+    callback(null, { err: err, data: data });
+    if (err) {
+      console.log(err);
+      context.fail(err);
+    } else {
+      console.log(data);
+      context.succeed(event);
+    }
+  });
 
 })
+
+function unCetifiedTemplate(payload) {
+    return {
+      Destination: {
+        ToAddresses: ["eni.olaniyan@collabralink.com"],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: `
+  Hi Stephnie,
+  
+  A State user has uncertiied their quarterly forms
+  Details:
+   - Username: ${payload.username}  || "username"
+     State:  ${payload.state}       || "state"
+     Role:  ${payload.role}         || "role"
+     Email:  ${payload.email}       || "email
+     
+  Regards,
+  Seds.
+  
+  `,
+          },
+        },
+        Subject: {
+          Data: `Uncerteried quartly form`,
+        },
+      },
+      Source: process.env.emailSource || "eniola.olaniyan",
+    };
+  }
