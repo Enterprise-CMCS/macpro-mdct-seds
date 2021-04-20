45e13740530b52a8c9a9c675ddb5929bb1854641
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-20 12:42:32 -0400

    fix syntax errors

--- services/app-api/handlers/notification/uncertified.js
+++ services/app-api/handlers/notification/uncertified.js
@@ -1,6 +1,6 @@
 import handler from "../../libs/handler-lib";
 var aws = require("aws-sdk");
-var ses = new aws.SES({ region: "us-east-1" });
+// var ses = new aws.SES({ region: "us-east-1" });
 
 /**
  * Handler responsible for sending notification to business users,
@@ -13,9 +13,9 @@ export const main = handler(async (event, context, callback) => {
     console.log("Warmed up!");
     return null;
   }
+  let data = JSON.parse(event.body);
   console.log(data);
-  console.log(event)
-//   let data = JSON.parse(event.body);
+  console.log(event);
 //   const email = unCetifiedTemplate(data);
 
 //   console.log(email);
@@ -24,35 +24,33 @@ export const main = handler(async (event, context, callback) => {
 //   await ses.sendEmail(email).promise();
 });
 
-
-
-function unCetifiedTemplate(payload) {
-  return {
-    Destination: {
-      ToAddresses: ["eolaniyan@collabralink.com"],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `
-  Hi Stephnie,
+// function unCetifiedTemplate(payload) {
+//   return {
+//     Destination: {
+//       ToAddresses: ["eolaniyan@collabralink.com"],
+//     },
+//     Message: {
+//       Body: {
+//         Text: {
+//           Data: `
+//   Hi Stephnie,
   
-  A State user has uncertiied their quarterly forms
-  Details:
-   - Username: ${payload.username}
-     State:  ${payload.state}
-     Role:  ${payload.role}
-     Email:  ${payload.email}
+//   A State user has uncertiied their quarterly forms
+//   Details:
+//    - Username: ${payload.username}
+//      State:  ${payload.state}
+//      Role:  ${payload.role}
+//      Email:  ${payload.email}
      
-  Regards,
-  Seds.
-  `,
-        },
-      },
-      Subject: {
-        Data: `Uncerteried quartly form`,
-      },
-    },
-    Source: process.env.emailSource || "eniola.olaniyan@cms.hhs.gov",
-  };
-}
+//   Regards,
+//   Seds.
+//   `,
+//         },
+//       },
+//       Subject: {
+//         Data: `Uncerteried quartly form`,
+//       },
+//     },
+//     Source: process.env.emailSource || "eniola.olaniyan@cms.hhs.gov",
+//   };
+// }
