320ebaa0d376a0ae0a21ca28fb2ec868b24a769c
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-20 16:06:40 -0400

    prettier

--- services/app-api/handlers/notification/businessUsers.js
+++ services/app-api/handlers/notification/businessUsers.js
@@ -4,7 +4,7 @@ import handler from "./../../libs/handler-lib";
  * that have not submitted their data at the end of a quarter - all reports in progress
  */
 
- export const main = handler(async (event, context) => {
+export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
--- services/app-api/handlers/notification/uncertified.js
+++ services/app-api/handlers/notification/uncertified.js
@@ -6,7 +6,7 @@ var ses = new aws.SES({ region: "us-east-1" });
  * Handler responsible for sending notification to business users,
  * each time a state takes an uncertify action on any of their quarterly forms
  */
- export const main = handler(async (event, context) => {
+export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
@@ -20,16 +20,26 @@ var ses = new aws.SES({ region: "us-east-1" });
   console.log(email);
   console.log(data);
 
-  ses.sendEmail(email, function (err, data) {
-    if (err) {
-      console.log(err);
-      context.fail(err);
-    } else {
-      console.log(data);
-      context.succeed(event);
-    }
-  });
-  return { message: "sucess, email sent" };
+  let sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+    .sendEmail(email)
+    .promise();
+  try {
+    const data = await sendPromise;
+    console.log(data.MessageId);
+  } catch (err) {
+    console.error(err, err.stack);
+  }
+
+  //   ses.sendEmail(email, function (err, data) {
+  //     if (err) {
+  //       console.log(err);
+  //       context.fail(err);
+  //     } else {
+  //       console.log(data);
+  //       context.succeed(event);
+  //     }
+  //   });
+  //   return { message: "sucess, email sent" };
 });
 
 function unCetifiedTemplate(payload) {
