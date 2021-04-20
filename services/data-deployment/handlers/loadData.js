8f92739f46b5866ba45108ba462c45d0fb67393e
mskalkar <mkalkar@collabralink.com>
2021-04-20 15:08:03 -0400

    Added few more tables to load in the script

--- services/data-deployment/handlers/loadData.js
+++ services/data-deployment/handlers/loadData.js
@@ -1,52 +1,47 @@
 // The script loads json data into Dynamodb tables
-//
-//var fs = require("fs");
-//
 var AWS = require("aws-sdk");
 var s3 = new AWS.S3();
 
 AWS.config.update({ region: "us-east-1" });
 
-//construct getParam
-var getParams = {
-  Bucket: "seds-s3bucket-data",
-  Key: "a.json",
-};
+function item_Upsert(bucketName, tableName)
+{
+  let fileName = tableName + ".json";
+  let getParams = {Key: fileName, Bucket: bucketName};
+  
+  console.log(getParams);
 
-//Fetch data from aws s3
-s3.getObject(getParams, function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    var testData = JSON.parse(data.Body.toString());
-    var documentClient = new AWS.DynamoDB.DocumentClient();
-    console.log("Loading data into DynamoDB");
-    console.log(testData);
-    //var testData = JSON.parse(fs.readFileSync('au.json', 'utf8'));
-    testData.forEach(function (rec) {
-      var params = {
-        TableName: "testLoad",
-        Item: {
-          userId: rec.userId,
-          password: rec.password,
-          username: rec.username,
-          firstName: rec.firstName,
-          lastName: rec.lastName,
-          email: rec.email,
-          isActive: rec.isActive,
-          role: rec.role,
-          states: rec.states,
-          dateJoined: rec.dateJoined,
-          lastLogin: rec.lastLogin,
-        },
-      };
-      documentClient.put(params, function (err, data) {
-        if (err) {
-          console.error("Can't add item !");
-        } else {
-          console.log("Succeeded adding an item");
-        }
+  //Fetch data from aws s3
+  s3.getObject(getParams, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var itemData = JSON.parse(data.Body.toString());
+      var documentClient = new AWS.DynamoDB.DocumentClient();
+      
+      console.log("Loading data into DynamoDB");
+      console.log(itemData);
+
+      itemData.forEach(function (record) {
+        var params = {
+          TableName: tableName,
+          Item: record
+        };
+        documentClient.put(params, function (err, data) {
+          if (err) {
+            console.error("Can't add item !");
+          } else {
+            console.log("Succeeded adding an item");
+          }
+        });
       });
-    });
-  }
-});
+    }
+  });
+  return;
+}
+
+let bucket = "seds-s3bucket-data";
+// Call function to insert items in Dynamodb tables 
+item_Upsert(bucket, "testLoad");
+item_Upsert(bucket, "testLoad2");
+
