77068925eb44cbc267bedf96c77e5fb2e04c668d
mskalkar <mkalkar@collabralink.com>
2021-04-20 18:17:25 -0400

    Fixed the data load script

--- services/data-deployment/handlers/loadData.js
+++ services/data-deployment/handlers/loadData.js
@@ -1,32 +1,32 @@
 // The script loads json data into Dynamodb tables
-var AWS = require("aws-sdk");
-var s3 = new AWS.S3();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 AWS.config.update({ region: "us-east-1" });
 
-function item_Upsert(bucketName, tableName) {
-  let fileName = tableName + ".json";
-  let getParams = { Key: fileName, Bucket: bucketName };
+itemUpsert = (bucketName, tableName) => {
+  const fileName = tableName + ".json";
+  const getParams = { Key: fileName, Bucket: bucketName };
 
   console.log(getParams);
 
   //Fetch data from aws s3
-  s3.getObject(getParams, function (err, data) {
+  s3.getObject(getParams, (err, data) => {
     if (err) {
       console.log(err);
     } else {
-      var itemData = JSON.parse(data.Body.toString());
-      var documentClient = new AWS.DynamoDB.DocumentClient();
+      const itemData = JSON.parse(data.Body.toString());
+      const documentClient = new AWS.DynamoDB.DocumentClient();
 
       console.log("Loading data into DynamoDB");
       console.log(itemData);
 
-      itemData.forEach(function (record) {
-        var params = {
+      itemData.forEach((record) => {
+        const params = {
           TableName: tableName,
           Item: record,
         };
-        documentClient.put(params, function (err, data) {
+        documentClient.put(params, (err, data) => {
           if (err) {
             console.error("Can't add item !");
           } else {
@@ -36,10 +36,9 @@ function item_Upsert(bucketName, tableName) {
       });
     }
   });
-  return;
-}
+};
 
-let bucket = "seds-s3bucket-data";
+const bucket = "seds-s3bucket-data";
 // Call function to insert items in Dynamodb tables
-item_Upsert(bucket, "testLoad");
-item_Upsert(bucket, "testLoad2");
+itemUpsert(bucket, "testLoad");
+itemUpsert(bucket, "testLoad2");
