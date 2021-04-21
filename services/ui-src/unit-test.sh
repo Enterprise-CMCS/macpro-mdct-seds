27fb7755c57ddfe1c83c82d9801bba07db538f47
dwhitecl <dwhite@collabralink.com>
2021-04-21 17:19:00 -0400

    updating unit test script

--- services/ui-src/unit-test.sh
+++ services/ui-src/unit-test.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-pushd ../../tests
-./test.sh
-popd
+install_deps() {
+  if [ "$CI" == "true" ]; then # If we're in a CI system
+    if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
+      yarn install --frozen-lockfile
+    fi
+  else # We're not in a CI system, let's yarn install
+    yarn install
+  fi
+}
