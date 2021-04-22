4811ace2abbfd12cd9e6caec36e2869e207fe22d
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-22 15:33:02 -0400

    build email selector

---
+++ services/app-api/handlers/notification/emailSelector.js
@@ -0,0 +1,62 @@
+import handler from "../../libs/handler-lib";
+var aws = require('aws_sdk');
+var lambda = new aws.Lambda({
+  region: "us-east-1"
+})
+
+export const main = handler(async (event, context) => {
+  // If this invokation is a prewarm, do nothing and return.
+  if (event.source == "serverless-plugin-warmup") {
+    console.log("Warmed up!");
+    return null;
+  }
+
+  lambda.invoke({
+    FunctionName: 'uncertified',
+    Payload: JSON.stringify(event, null, 2) // pass params
+  }, function(error, data) {
+    if (error) {
+      context.done('error', error);
+    }
+    if(data.Payload){
+     context.succeed(data.Payload)
+    }
+  });
+
+    // If this invokation is a prewarm, do nothing and return.
+    if (event.source == "serverless-plugin-warmup") {
+      console.log("Warmed up!");
+      return null;
+    }
+});
+
+function uncetifiedTemplate(payload) {
+  return {
+    Destination: {
+      ToAddresses: ["eolaniyan@collabralink.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: `
+  Hi Stephnie,
+  
+  A State user has uncertiied their quarterly forms
+  Details:
+   - Username: ${payload.username}
+     State:  ${payload.state}
+     Role:  ${payload.role}
+     Email:  ${payload.email}
+     
+  Regards,
+  Seds.
+  `,
+        },
+      },
+      Subject: {
+        Data: `Uncerteried quartly form`,
+      },
+    },
+    Source: process.env.emailSource || "eniola.olaniyan@cms.hhs.gov",
+  };
+}
--- services/app-api/handlers/notification/templateSelector.js
+++
@@ -1,9 +0,0 @@
-import handler from "./../../libs/handler-lib";
-
-export const main = handler(async (event, context) => {
-    // If this invokation is a prewarm, do nothing and return.
-    if (event.source == "serverless-plugin-warmup") {
-      console.log("Warmed up!");
-      return null;
-    }
-});
