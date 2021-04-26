69d413bbe9f5ea9c2cf6a64afcfd54f592b27233
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-26 09:18:21 -0400

    fix linting #2

--- services/app-api/handlers/notification/uncertified.js
+++ services/app-api/handlers/notification/uncertified.js
@@ -8,6 +8,7 @@ export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
+    console.log("Warmed up!");
     return null;
   }
   let data = JSON.parse(event.body);
