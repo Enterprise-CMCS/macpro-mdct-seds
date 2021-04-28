f7e29457b24376fbef27a4918779ab66490a1efe
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-28 14:26:15 -0400

    fix syntax error

--- services/ui-src/src/components/CertificationTab/CertificationTab.js
+++ services/ui-src/src/components/CertificationTab/CertificationTab.js
@@ -42,32 +42,33 @@ const CertificationTab = ({
   const submitUncertify = async () => {
     if (window.confirm("Are you sure you want to uncertify this report?")) {
       uncertify();
-      await stateUsersEmail()
+      await stateUsersEmail();
+      await businessUsersEmail();
       // await sendEmailtoBo();
       setprovisionalButtonStatus(false);
       setfinalButtonStatus(false);
     }
   };
 
-  const sendEmailtoBo = async () => {
-    const authUser = await Auth.currentSession();
-    const userEmail = authUser.idToken.payload.email;
-    var datetime = new Date().getTime();
-    const currentUser = await obtainUserByEmail({
-      email: userEmail,
-    });
-    let userObj = currentUser["Items"];
-    userObj.map(async userInfo => {
-      if (userInfo.role === "state") {
-        let emailObj = {
-          states: userInfo.states,
-          date: datetime,
-          username: userInfo.username
-        }
-        await sendUncertifyEmail(emailObj);
-      } 
-    });
-  }
+  // const sendEmailtoBo = async () => {
+  //   const authUser = await Auth.currentSession();
+  //   const userEmail = authUser.idToken.payload.email;
+  //   var datetime = new Date().getTime();
+  //   const currentUser = await obtainUserByEmail({
+  //     email: userEmail,
+  //   });
+  //   let userObj = currentUser["Items"];
+  //   userObj.map(async userInfo => {
+  //     if (userInfo.role === "state") {
+  //       let emailObj = {
+  //         states: userInfo.states,
+  //         date: datetime,
+  //         username: userInfo.username
+  //       }
+  //       await sendUncertifyEmail(emailObj);
+  //     } 
+  //   });
+  // }
 
   let certifyText;
   if (isFinal) {
