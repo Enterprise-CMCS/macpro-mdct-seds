1cc36a38e59436d54f331f2527a425a1aa210b80
Alexis Woodbury <awoodbury@collabralink.com>
2021-04-27 20:02:05 -0400

    remove unused useeffect and usestate from formfooter

--- services/ui-src/src/components/FormFooter/FormFooter.js
+++ services/ui-src/src/components/FormFooter/FormFooter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Grid, GridContainer, Button } from "@trussworks/react-uswds";
