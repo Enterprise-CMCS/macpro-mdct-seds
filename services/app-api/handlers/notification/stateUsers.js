fa6ae0dab2f8a0e818e877f981d3ecee247afb40
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-29 16:02:57 -0400

    fix syntax errors

--- services/app-api/handlers/notification/stateUsers.js
+++ services/app-api/handlers/notification/stateUsers.js
@@ -11,7 +11,7 @@ import dynamoDb from "./../../libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
   // let data = JSON.parse(event.body);
 
-  const test = getStsateUsersEmail();
+  await getStateUsersEmail();
 
   // const email = stateTemplate(data)
   // .sendEmail(email)
@@ -46,7 +46,7 @@ async function getStateUsersEmail() {
   const payload = result["Items"];
   payload.map((userInfo) => {
     if (userInfo.email) {
-      stateUsers.push(userInfo.email);
+      stateUsersEmail.push(userInfo.email);
     }
   });
   return stateUsersEmail;
@@ -76,69 +76,66 @@ async function getStateUsersEmail() {
 //   const payload = result.Items;
 //   payload.map(stateInfo => {
 //     // pulled the state from each state forms and pushed into array
-//     UncertifiedstateList.push(stateInfo.program_code) 
+//     UncertifiedstateList.push(stateInfo.program_code)
 //   });
-//   let filteredStateList = UncertifiedstateList.filter(function(elem, index, self) { 
+//   let filteredStateList = UncertifiedstateList.filter(function(elem, index, self) {
 //     // filter the state list so we dont have duplicates
 //     return index === self.indexOf(elem);
 //   });
 //   return filteredStateList;
+// };
+
+// async function stateTemplate() {
+//   const sendToEmail = await getStateUsersEmail();
+//   // const UncerteriedStateList = await getUncertifiedStates();
+//   const fromEmail = "eniola.olaniyan@cms.hhs.gov";
+
+//   const recipient = {
+//     TO: sendToEmail,
+//     SUBJECT: "Reminder: [State] FFY[Fiscal Year] Q[Quarter] SEDS Enrollment Data Overdue",
+//     FROM: fromEmail,
+//     MESSAGE: `
+//     Hello [State],
+
+//     We are reaching out to check on the status of [State]'s FFY[Fiscal Year]
+//     Q[Quarter] child enrollment data submission in the Statistical Enrollment Data System (SEDS).
+
+//     FFY[Fiscal Year] Q[Quarter] reporting of enrollment data to the SEDS was
+//     due on [DUE DATE]. Our records indicate that [State] has not yet submitted
+//     the required enrollment data to SEDS at this time. Please let us know when
+//     we can expect your submission.
+
+//     If your state has any other outstanding Quarter(s) of data, please submit
+//     that information along with your FFY[Fiscal Year] Q[Quarter] data.
+
+//     If your state allows retroactive eligibility, the initial enrollment reports will be
+//     considered preliminary and your state will have the opportunity to submit final reports
+//     thirty (30) days after the end of the next quarter (with next quarter’s preliminary report).
+//     The final reports should include information about children whose eligibility was
+//     retroactive to the earlier quarter.
+
+//     If you have any questions,
+//     please send an email to: MDCTHelp@cms.hss.gov
+
+//     Thank you in advance for your prompt response to this message.
+
+//     Regards,
+//     CMS SEDS TA Team.
+
+//     `,
+//   };
+//   return {
+//     Destination: { ToAddresses: recipient.TO}, // All Active Users With the “state” Role Assigned to the State
+//     Message: {
+//       Body: {
+//         Text: {
+//           Data: recipient.MESSAGE
+//         },
+//       },
+//       Subject: {
+//         Data: recipient.SUBJECT
+//       },
+//     },
+//     Source: recipient.FROM
+//   };
 // }
-
-async function stateTemplate(payload) {
-  const sendToEmail = await getStateUsersEmail();
-  // const UncerteriedStateList = await getUncertifiedStates();
-
-
-
-  const fromEmail = "eniola.olaniyan@cms.hhs.gov";
-
-  const recipient = {
-    TO: sendToEmail,
-    SUBJECT: "Reminder: [State] FFY[Fiscal Year] Q[Quarter] SEDS Enrollment Data Overdue",
-    FROM: fromEmail,
-    MESSAGE: `
-    Hello [State],
-
-    We are reaching out to check on the status of [State]'s FFY[Fiscal Year]
-    Q[Quarter] child enrollment data submission in the Statistical Enrollment Data System (SEDS).
-
-    FFY[Fiscal Year] Q[Quarter] reporting of enrollment data to the SEDS was
-    due on [DUE DATE]. Our records indicate that [State] has not yet submitted
-    the required enrollment data to SEDS at this time. Please let us know when
-    we can expect your submission.
-
-    If your state has any other outstanding Quarter(s) of data, please submit
-    that information along with your FFY[Fiscal Year] Q[Quarter] data.
-
-    If your state allows retroactive eligibility, the initial enrollment reports will be
-    considered preliminary and your state will have the opportunity to submit final reports
-    thirty (30) days after the end of the next quarter (with next quarter’s preliminary report).
-    The final reports should include information about children whose eligibility was
-    retroactive to the earlier quarter.
-
-    If you have any questions,
-    please send an email to: MDCTHelp@cms.hss.gov
-
-    Thank you in advance for your prompt response to this message.
-
-    Regards,
-    CMS SEDS TA Team.
-
-    `,
-  };
-  return {
-    Destination: { ToAddresses: recipient.TO}, // All Active Users With the “state” Role Assigned to the State
-    Message: {
-      Body: {
-        Text: {
-          Data: recipient.MESSAGE
-        },
-      },
-      Subject: {
-        Data: recipient.SUBJECT
-      },
-    },
-    Source: recipient.FROM
-  };
-}
