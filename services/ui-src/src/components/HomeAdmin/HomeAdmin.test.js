932aed3a2a20835390c70965038e6a6cc05e4e4f
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-05-05 12:01:50 -0400

    Remove unnecessary tests, add provider and store

--- services/ui-src/src/components/HomeAdmin/HomeAdmin.test.js
+++ services/ui-src/src/components/HomeAdmin/HomeAdmin.test.js
@@ -2,14 +2,27 @@ import React from "react";
 import { mount } from "enzyme";
 import HomeAdmin from "./HomeAdmin";
 import { BrowserRouter, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import fullStoreMock from "../../provider-mocks/fullStoreMock";
+const mockStore = configureStore([]);
 
 describe("Tests for HomeAdmin.js", () => {
   let wrapper;
+  let store;
+
+  const realUseState = React.useState;
+  const mockInitialState = "admin";
+  // Mock useState before rendering your component to set initial state values
 
   beforeEach(() => {
+    store = mockStore(fullStoreMock);
+
     wrapper = mount(
       <BrowserRouter>
-        <HomeAdmin />
+        <Provider store={store}>
+          <HomeAdmin />
+        </Provider>
       </BrowserRouter>
     );
   });
@@ -20,18 +33,5 @@ describe("Tests for HomeAdmin.js", () => {
 
   test("Ensure links are visible", () => {
     expect(wrapper.containsMatchingElement(<Link />));
-  });
-
-  test("Ensure view/edit user link exists", () => {
-    let anchor = wrapper
-      .find(".user-view-edit")
-      .children()
-      .find("a")
-      .prop("href");
-    expect(anchor).toEqual("/users");
-  });
-  test("Ensure add user link exists", () => {
-    let anchor = wrapper.find(".user-add").children().find("a").prop("href");
-    expect(anchor).toEqual("/users/add");
   });
 });
