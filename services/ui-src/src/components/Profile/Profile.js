9a7f5af41b1919e8325f8e1b8e5b98e180fae45b
eolaniyan052 <eolaniyan@collabralink.com>
2021-05-10 15:26:06 -0400

    test

--- services/ui-src/src/components/Profile/Profile.js
+++ services/ui-src/src/components/Profile/Profile.js
@@ -28,6 +28,9 @@ export default function Profile({ user }) {
     const onLoad = async () => {
       try {
         const AuthUserInfo = (await Auth.currentSession()).getIdToken();
+        console.log("This i sthe current user: ", AuthUserInfo);
+
+
         const currentUserInfo = await obtainUserByEmail({
           email: AuthUserInfo.attributes.email
         });
