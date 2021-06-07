6f16f64cc10b8710f57e971bc42d5a32131d80d5
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-06-07 15:08:51 -0400

    Remove unused var

--- services/app-api/handlers/forms/post/generateQuarterForms.js
+++ services/app-api/handlers/forms/post/generateQuarterForms.js
@@ -131,28 +131,23 @@ export const main = handler(async (event, context) => {
         const stateFormID = `${currentState}-${specifiedYear}-${specifiedQuarter}-${currentForm}`;
 
         // Setup params for insert
-        const insertParams = {
-          TableName:
-            process.env.FORM_ANSWERS_TABLE_NAME ??
-            process.env.FormAnswersTableName,
-          Item: {
-            answer_entry: answerEntry,
-            age_range: currentAgeRangeLabel,
-            rangeId: currentAgeRangeId,
-            question: questionID,
-            state_form: stateFormID,
-            last_modified_by: "seed",
-            created_date: new Date().toISOString(),
-            rows: allQuestions[question].rows,
-            last_modified: new Date().toISOString(),
-            created_by: "seed",
-          },
-        };
-        console.log(
-          "FORM_ANSWERS_TABLE_NAME",
-          process.env.FORM_ANSWERS_TABLE_NAME
-        );
-        console.log("FormAnswersTableName", process.env.FormAnswersTableName);
+        // const insertParams = {
+        //   TableName:
+        //     process.env.FORM_ANSWERS_TABLE_NAME ??
+        //     process.env.FormAnswersTableName,
+        //   Item: {
+        //     answer_entry: answerEntry,
+        //     age_range: currentAgeRangeLabel,
+        //     rangeId: currentAgeRangeId,
+        //     question: questionID,
+        //     state_form: stateFormID,
+        //     last_modified_by: "seed",
+        //     created_date: new Date().toISOString(),
+        //     rows: allQuestions[question].rows,
+        //     last_modified: new Date().toISOString(),
+        //     created_by: "seed",
+        //   },
+        // };
 
         try {
           // await dynamoDb.put(insertParams);
