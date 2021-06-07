8719f1c648326116849df0c477b5cf2c6826cf84
eolaniyan052 <eolaniyan@collabralink.com>
2021-06-07 12:32:13 -0400

    fix bug with notification and update template

--- services/app-api/handlers/notification/stateUsers.js
+++ services/app-api/handlers/notification/stateUsers.js
@@ -57,12 +57,11 @@ async function certifiedStateUsersEmail() {
 async function stateUsersTemplate() {
   // Email of state users whose state isnt certified yet
   const stateUsersToEmail = await certifiedStateUsersEmail();
-  console.log("stateUsersToEmail", stateUsersToEmail);
   const fromEmail = "mdct@cms.hhs.gov";
   let todayDate = new Date().toISOString().split("T")[0];
 
   const recipient = {
-    TO: ["stephanie.coppel@cms.hhs.gov", "olaniyan86@gmail.com"], // stateUsersToEmail,
+    TO: stateUsersToEmail,
     SUBJECT: `Reminder: [State] FFY${year} Q${quarter} SEDS Enrollment Data Overdue`,
     FROM: fromEmail,
     MESSAGE: `
