9bd8486dee17db7af1c1969a15da9dc98e9dc704
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-06-07 15:21:07 -0400

    Remove unused var

--- services/app-api/handlers/forms/post/generateQuarterForms.js
+++ services/app-api/handlers/forms/post/generateQuarterForms.js
@@ -101,7 +101,7 @@ export const main = handler(async (event, context) => {
         },
       };
       try {
-        await dynamoDb.put(insertFormParams);
+        await dynamoDb.post(insertFormParams);
         return {
           status: 200,
           message: "State Form addition successful",
