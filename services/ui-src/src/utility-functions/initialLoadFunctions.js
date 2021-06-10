c2d75fa1d94263fddb196b0bce2572a538f7641c
Andrew Adcock <76052098+andrewcollabralink@users.noreply.github.com>
2021-06-10 16:17:01 -0400

    Login bug (#367)
    * Correct issues with login
    
    * Correct infinite loop, clean up

--- services/ui-src/src/utility-functions/initialLoadFunctions.js
+++ services/ui-src/src/utility-functions/initialLoadFunctions.js
@@ -7,7 +7,7 @@ export async function ascertainUserPresence(user) {
   });
 
   const userObject = {
-    username: user.username,
+    username: user.username ?? user.attributes.email,
     email: user.attributes.email,
     firstName: user.attributes.given_name,
     lastName: user.attributes.family_name,
@@ -29,36 +29,33 @@ export async function ascertainUserPresence(user) {
 const checkRoleFromStore = async () => {
   let userRole;
   const currentUser = (await Auth.currentSession()).getIdToken();
+
   const {
     payload: { email }
   } = currentUser;
+
   const existingUser = await obtainUserByEmail({ email });
+
+  if (existingUser === false) {
+    return false;
+  }
   const userdata = existingUser["Items"];
+
   userdata.map(async userInfo => {
     userRole = userInfo.role;
   });
+
   return userRole;
 };
 
 export const determineRole = async specRole => {
   const userStoreRole = await checkRoleFromStore();
+
   const roleArray = ["admin", "business", "state"];
   let role;
 
   if (userStoreRole && roleArray.includes(userStoreRole)) {
-    switch (userStoreRole) {
-      case "state":
-        role = "state";
-        break;
-      case "business":
-        role = "business";
-        break;
-      case "admin":
-        role = "admin";
-        break;
-      default:
-        break;
-    }
+    role = userStoreRole;
   } else if (specRole) {
     if (
       specRole.includes("CHIP_D_USER_GROUP_ADMIN") ||
@@ -74,5 +71,6 @@ export const determineRole = async specRole => {
       role = "state";
     }
   }
+
   return role;
 };
