8ed5a154434e0ecc1cf3484e7c008e40882cfb91
eolaniyan052 <eolaniyan@collabralink.com>
2021-06-10 08:30:17 -0400

    remove unused declarrtion

--- services/ui-src/src/components/HomeAdmin/HomeAdmin.test.js
+++ services/ui-src/src/components/HomeAdmin/HomeAdmin.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 import HomeAdmin from "./HomeAdmin";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import fullStoreMock from "../../provider-mocks/fullStoreMock";
