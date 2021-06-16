6a862e81c12cd08e188c647215c18b68d73c1d6b
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-06-16 08:58:10 -0400

    Prettier tweaks

--- services/app-api/handlers/notification/stateUsers.js
+++ services/app-api/handlers/notification/stateUsers.js
@@ -32,17 +32,17 @@ export const main = handler(async (event, context) => {
 
 function getQuarter() {
   let d = new Date();
-  let m = Math.floor(d.getMonth()/3) + 2;
-  return m > 4? m - 4 : m;
+  let m = Math.floor(d.getMonth() / 3) + 2;
+  return m > 4 ? m - 4 : m;
 }
 const quarter = getQuarter();
-const year =  new Date().getFullYear();
+const year = new Date().getFullYear();
 
 // returns a list of state users emails whose state isnt fully certified
 async function certifiedStateUsersEmail() {
   const allStateEmails = await getUsersEmailByRole("state");
   const uncertifiedStateList = await getUncertifiedStates(year, quarter);
-  console.log("uncertified states: ",uncertifiedStateList);
+  console.log("uncertified states: ", uncertifiedStateList);
 
   let stateUsersToEmail = [];
   allStateEmails.map((e) => {
