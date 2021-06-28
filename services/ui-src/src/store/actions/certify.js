2fd86fc73544f5f05bb0df83b335077c556e4431
eolaniyan052 <eolaniyan@collabralink.com>
2021-06-28 12:01:55 -0400

    .

--- services/ui-src/src/store/actions/certify.js
+++ services/ui-src/src/store/actions/certify.js
@@ -1,4 +1,6 @@
 import { API } from "aws-amplify";
+import {getUsername} from "../reducers/singleForm";
+
 // ACTION TYPES
 export const CERTIFY_AND_SUBMIT_FINAL = "CERTIFY_AND_SUBMIT_FINAL";
 export const CERTIFY_AND_SUBMIT_PROVISIONAL = "CERTIFY_AND_SUBMIT_PROVISIONAL";
@@ -28,8 +30,7 @@ export const setUncertify = username => {
 
 // THUNK FUNCTIONS
 export const certifyAndSubmitFinal = () => async dispatch => {
-  const { data } = await API.post("mdct-seds", "/users/get/username", {});
-  const username = data.username;
+  const username = await getUsername();
   try {
     dispatch(setFinalCertify(username));
   } catch (error) {
@@ -38,8 +39,7 @@ export const certifyAndSubmitFinal = () => async dispatch => {
 };
 
 export const certifyAndSubmitProvisional = () => async dispatch => {
-  const { data } = await API.post("mdct-seds", "/users/get/username", {});
-  const username = data.username;
+  const username = await getUsername();
   try {
     dispatch(setProvisionalCertify(username));
   } catch (error) {
@@ -48,7 +48,6 @@ export const certifyAndSubmitProvisional = () => async dispatch => {
 };
 
 export const uncertify = () => async dispatch => {
-  const { data } = await API.post("mdct-seds", "/users/get/username", {});
-  const username = data.username;
+  const username = await getUsername();
   dispatch(setUncertify(username));
 };
