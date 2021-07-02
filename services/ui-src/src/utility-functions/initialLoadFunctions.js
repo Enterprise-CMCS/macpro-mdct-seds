c49c23ff87dd90768449a47875a3b56b263ea559
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-07-02 10:36:36 -0400

    Add debug code

--- services/ui-src/src/utility-functions/initialLoadFunctions.js
+++ services/ui-src/src/utility-functions/initialLoadFunctions.js
@@ -6,7 +6,7 @@ export async function ascertainUserPresence(user) {
   const existingUser = await obtainUserByEmail({
     email: user.attributes.email
   });
-
+  console.log("zzzUserObject", userObject);
   const userObject = {
     username: user.username ?? user.attributes.email,
     email: user.attributes.email,
