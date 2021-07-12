3143ec34b1355b4f1babef4fefa9775095c4122c
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-07-12 13:39:41 -0400

    Add new handlers, for gettting templates, years, and adding/editing new

---
+++ services/app-api/handlers/form-templates/post/getFormTemplateYears.js
@@ -0,0 +1,28 @@
+import handler from "../../../libs/handler-lib";
+import dynamoDb from "../../../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  // If this invokation is a prewarm, do nothing and return.
+  if (event.source == "serverless-plugin-warmup") {
+    console.log("Warmed up!");
+    return null;
+  }
+
+  const params = {
+    TableName:
+      process.env.FORM_TEMPLATES_TABLE_NAME ??
+      process.env.FormTemplatesTableName,
+    ExpressionAttributeNames: {
+      "#theYear": "year",
+    },
+    ProjectionExpression: "#theYear",
+  };
+
+  const result = await dynamoDb.scan(params);
+
+  if (result.Count === 0) {
+    throw new Error("Form type query failed");
+  }
+
+  return result.Items;
+});
---
+++ services/app-api/handlers/form-templates/post/updateCreateFormTemplate.js
@@ -0,0 +1,53 @@
+import handler from "../../../libs/handler-lib";
+import dynamoDb from "../../../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  // If this invocation is a prewarm, do nothing and return.
+  if (event.source === "serverless-plugin-warmup") {
+    console.log("Warmed up!");
+    return null;
+  }
+
+  const isJsonString = (jsonString) => {
+    try {
+      // try to stringify and parse the incoming data to verify if valid json
+      let o = JSON.parse(JSON.stringify(jsonString));
+
+      if (o && typeof o === "object") {
+        return o;
+      }
+    } catch (e) {}
+
+    return false;
+  };
+
+  const data = JSON.parse(event.body);
+
+  if (!data.year || !data.template) {
+    return {
+      status: 422,
+      message: `Please specify both a year and a template`,
+    };
+  }
+  console.log("zzzData.template", data.template[0]);
+  if (!isJsonString(data.template[0])) {
+    return {
+      status: 400,
+      message: `Invalid JSON. Please review your template.`,
+    };
+  }
+
+  const params = {
+    TableName:
+      process.env.FORM_TEMPLATES_TABLE_NAME ??
+      process.env.FormTemplatesTableName,
+    Item: {
+      year: parseInt(data.year),
+      template: data.template,
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return `Template updated for ${data.year}!`;
+});
