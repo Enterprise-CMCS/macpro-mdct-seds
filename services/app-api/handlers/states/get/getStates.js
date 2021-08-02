1b9d749682cd9075f37935a6e3cb8ba6d27d1cd6
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-08-02 08:53:52 -0400

    Add code back, since it made no difference to codeclimate

--- services/app-api/handlers/states/get/getStates.js
+++ services/app-api/handlers/states/get/getStates.js
@@ -2,6 +2,12 @@ import handler from "../../../libs/handler-lib";
 import dynamoDb from "../../../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
+  // If this invokation is a prewarm, do nothing and return.
+  if (event.source == "serverless-plugin-warmup") {
+    console.log("Warmed up!");
+    return null;
+  }
+
   const params = {
     TableName: process.env.StatesTableName,
     Select: "ALL_ATTRIBUTES",
