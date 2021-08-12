1b6ce2ad0ef30ad88b357455f453ff345b1e3b74
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-08-12 12:50:44 -0400

    Add form template and unmarshall data

--- services/app-api/handlers/kafka/post/postKafkaData.js
+++ services/app-api/handlers/kafka/post/postKafkaData.js
@@ -1,3 +1,5 @@
+import AWS from "aws-sdk";
+
 const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
   clientId: "dynamodb",
@@ -17,25 +19,26 @@ exports.handler = async (event) => {
 
   const streamARN = String(event.Records[0].eventSourceARN.toString());
   let topicName = "aws.mdct.seds.cdc.";
+  let version = ".v0";
 
   if (streamARN.includes("age-ranges")) {
-    topicName = topicName + "age-ranges";
+    topicName = topicName + "age-ranges" + version;
   } else if (streamARN.includes("auth-user")) {
-    topicName = topicName + "auth-user";
+    topicName = topicName + "auth-user" + version;
   } else if (streamARN.includes("form-answers")) {
-    topicName = topicName + "form-answers";
+    topicName = topicName + "form-answers" + version;
   } else if (streamARN.includes("form-questions")) {
-    topicName = topicName + "form-questions";
+    topicName = topicName + "form-questions" + version;
   } else if (streamARN.includes("form-templates")) {
-    topicName = topicName + "form-templates";
+    topicName = topicName + "form-templates" + version;
   } else if (streamARN.includes("forms")) {
-    topicName = topicName + "forms";
+    topicName = topicName + "forms.v0";
   } else if (streamARN.includes("state-forms")) {
-    topicName = topicName + "state-forms";
+    topicName = topicName + "state-forms" + version;
   } else if (streamARN.includes("states")) {
-    topicName = topicName + "states";
+    topicName = topicName + "states" + version;
   } else if (streamARN.includes("status")) {
-    topicName = topicName + "status";
+    topicName = topicName + "status" + version;
   }
 
   console.log("EVENT INFO HERE", event);
@@ -47,7 +50,7 @@ exports.handler = async (event) => {
           messages: [
             {
               key: "key4",
-              value: JSON.stringify(record.dynamodb, null, 2),
+              value: AWS.DynamoDB.Converter.unmarshall(record.dynamodb),
               partition: 0,
             },
           ],
