37c6c4fb8d8184df8d4c67ab949f9547ccbb8b24
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-08-16 15:54:31 -0400

    Fix bug with too many forms showing

--- services/ui-src/src/utility-functions/dbFunctions.js
+++ services/ui-src/src/utility-functions/dbFunctions.js
@@ -1,9 +1,10 @@
 import { getStateForms } from "../libs/api";
 
 // Recursively call getStateForms until all values are returned
-// Init response array
-const returnItems = [];
-export const recursiveGetStateForms = async data => {
+export const recursiveGetStateForms = async (data, currentData) => {
+  // Get array data from previous iteration or set to empty array
+  let returnItems = currentData ?? [];
+
   let formData = {
     state: data.state,
     year: data.year,
@@ -11,26 +12,13 @@ export const recursiveGetStateForms = async data => {
     startKey: data.startKey ?? false
   };
 
-  // Pull data
   let response = await getStateForms(formData);
 
-  // If already in array, don't add it
-  // Check for (db) matching value of current item state_form
-  for (let i in response.Items) {
-    let hasItem = returnItems.some(
-      e => e.state_form === response.Items[i].state_form
-    );
+  returnItems = returnItems.concat(response.Items);
 
-    // If not already in array, add it
-    if (!hasItem) {
-      returnItems.push(response.Items[i]);
-    }
-  }
-
-  if (response.LastEvaluatedKey !== undefined) {
-    // Set startKey to lastevaluated, for recursion
+  if (response.LastEvaluatedKey) {
     data.startKey = response.LastEvaluatedKey;
-    return await recursiveGetStateForms(data);
+    return await recursiveGetStateForms(data, returnItems);
   }
   return returnItems;
 };
