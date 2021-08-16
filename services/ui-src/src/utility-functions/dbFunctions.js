b37c50e2a3ff29f1a3f72776b4172b05256f146b
Andrew Adcock <andrew.adcock@cms.hhs.gov>
2021-08-16 10:19:30 -0400

    Remove duplicates

--- services/ui-src/src/utility-functions/dbFunctions.js
+++ services/ui-src/src/utility-functions/dbFunctions.js
@@ -1,22 +1,37 @@
 import { getStateForms } from "../libs/api";
 
 // Recursively call getStateForms until all values are returned
-// Required data: state, year, quarter, startKey(optional)
+// Init response array
+const returnItems = [];
 export const recursiveGetStateForms = async data => {
-  // Init response array
-  const returnItems = [];
+  // If startKey has value, use it
+  // this is the LastEvaluatedKey from the previous scan
+  let startKey = false;
+  if (data.startKey) {
+    startKey = data.startKey;
+  }
 
-  // Set up initial retrieval (no start key)
+  // Sewt
   let response = await getStateForms({
     state: data.state,
     year: data.year,
-    quarter: data.quarter
+    quarter: data.quarter,
+    startKey: startKey
   });
 
-  // Add just the Items to the response array
-  returnItems.push(...response.Items);
+  // If already in array, don't add it
+  // Check for (db) matching value of current item state_form
+  for (let i in response.Items) {
+    let hasItem = returnItems.some(
+      e => e.state_form === response.Items[i].state_form
+    );
 
-  // If LastEvaluatedKey is NOT undefined, there are more records to retrieve. So call the function again.
+    // If not already in array, add it
+    if (!hasItem) {
+      returnItems.push(response.Items[i]);
+    }
+  }
+
   if (response.LastEvaluatedKey !== undefined) {
     return await recursiveGetStateForms({
       state: data.state,
