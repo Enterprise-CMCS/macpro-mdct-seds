406e6239c3f3430c39e1037d7ffcfdbe79f7242e
Andrew Adcock <76052098+andrewcollabralink@users.noreply.github.com>
2021-08-17 06:50:00 -0400

    OY2 11155 - Scan failing to retrieve all records (#430)
    * Update state-forms table when saving
    
    * Allows update of total counts
    
    * Add interface to run enrollment updates
    
    * Cleanup
    
    * Capitalization
    
    * Remove unused call
    
    * Add fallback for tablenames
    
    * Batch write to state-forms table
    
    * Set timeout to 1000
    
    * Set timeout to 900
    
    * Clean up
    
    * Split monolith handler into three separate calls
    
    * Increase obtainAnswerEntriesFromForms timeout to 900
    
    * Correct typo in table name
    
    * Change handler name for easier tracing
    
    * Remove unused handler
    
    * Batch obtainAnswerEntriesFromForms queries
    
    * Recursively call scan for getFormsList.js
    
    * More recursion to react side of things
    
    * Remove duplicates
    
    * Tidy up
    
    * Attempt to simplify
    
    * Fix bug with too many forms showing

---
+++ services/ui-src/src/utility-functions/dbFunctions.js
@@ -0,0 +1,24 @@
+import { getStateForms } from "../libs/api";
+
+// Recursively call getStateForms until all values are returned
+export const recursiveGetStateForms = async (data, currentData) => {
+  // Get array data from previous iteration or set to empty array
+  let returnItems = currentData ?? [];
+
+  let formData = {
+    state: data.state,
+    year: data.year,
+    quarter: data.quarter,
+    startKey: data.startKey ?? false
+  };
+
+  let response = await getStateForms(formData);
+
+  returnItems = returnItems.concat(response.Items);
+
+  if (response.LastEvaluatedKey) {
+    data.startKey = response.LastEvaluatedKey;
+    return await recursiveGetStateForms(data, returnItems);
+  }
+  return returnItems;
+};
