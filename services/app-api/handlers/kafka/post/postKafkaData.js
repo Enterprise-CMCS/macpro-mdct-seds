fb6c8ae41019a8985caa870d35a42d11ccd47d63
Berry Davenport <bdavenport@collabralink.com>
2021-08-19 11:50:30 -0400

    Fix key mapping

--- services/app-api/handlers/kafka/post/postKafkaData.js
+++ services/app-api/handlers/kafka/post/postKafkaData.js
@@ -40,34 +40,25 @@ exports.handler = async (event) => {
     if (event.Records) {
       for (const record of event.Records) {
         const streamARN = String(record.eventSourceARN.toString());
-        let pk;
 
         if (streamARN.includes("age-ranges")) {
           topicName = topic("age-ranges");
-          pk = "ageRange";
         } else if (streamARN.includes("auth-user")) {
           topicName = topic("auth-user");
-          pk = "userId";
         } else if (streamARN.includes("form-answers")) {
           topicName = topic("form-answers");
-          pk = "answer_entry";
         } else if (streamARN.includes("form-questions")) {
           topicName = topic("form-questions");
-          pk = "question";
         } else if (streamARN.includes("form-templates")) {
           topicName = topic("form-templates");
-          pk = "year";
         } else if (streamARN.includes("forms")) {
           topicName = topic("forms");
         } else if (streamARN.includes("state-forms")) {
           topicName = topic("state-forms");
-          pk = "state_form";
         } else if (streamARN.includes("states")) {
           topicName = topic("states");
-          pk = "state_id";
         } else if (streamARN.includes("status")) {
           topicName = topic("status");
-          pk = "status";
         }
 
         const dynamoRecord = AWS.DynamoDB.Converter.unmarshall(record.dynamodb);
@@ -77,7 +68,7 @@ exports.handler = async (event) => {
           topic: topicName,
           messages: [
             {
-              key: dynamoRecord.dynamodb.NewImage[pk],
+              key: Object.values(dynamoRecord.Keys).join('#'),
               value: dynamoStringified,
               partition: 0,
             },
