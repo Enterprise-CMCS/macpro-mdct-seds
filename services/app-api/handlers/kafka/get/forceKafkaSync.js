676f80d1f25c1220676a8f2687a3121d776f9449
Hasan Farook <hasanfar@gmail.com>
2021-08-23 16:21:44 -0400

    fixed lint issues

--- services/app-api/handlers/kafka/get/forceKafkaSync.js
+++ services/app-api/handlers/kafka/get/forceKafkaSync.js
@@ -1,7 +1,6 @@
 import handler from "../../../libs/handler-lib";
 import dynamoDb from "../../../libs/dynamodb-lib";
-import chunk from "lodash/chunk";
-import { table } from "console";
+// import chunk from "lodash/chunk";
 
 const scanTable = async (
     tableName,
@@ -19,62 +18,62 @@ const scanTable = async (
         keepSearching = false;
         return [null, keepSearching, results];
     }
-}
+};
 
-const batchWrite = async (tableName, items) => {
+// const batchWrite = async (tableName, items) => {
 
-    const itemChunks = chunk(items, 25);
-    for (const chunk in itemChunks) {
+//     const itemChunks = chunk(items, 25);
+//     for (const chunk in itemChunks) {
 
-        const itemArray = chunk.map(item => {
-            // add item.lastSynced = current time with reqd format
-            return {
-                PutRequest: {
-                    Item: item
-                }
-            };
-        });
+//         const itemArray = chunk.map(item => {
+//             // add item.lastSynced = current time with reqd format
+//             return {
+//                 PutRequest: {
+//                     Item: item
+//                 }
+//             };
+//         });
 
-        let requestItems = {};
-        requestItems[tableName] = itemArray;
-        const params = {
-            RequestItems: requestItems
-        }
-        const { UnprocessedItems } = await dynamoDb.batchWrite(params);
-        // Todo: process UnprocessedItems
-        let failedItems = [];
-        for (const item of items) {
-            const { UnprocessedItems } = await dynamoDb.batchWrite({
-                RequestItems: {
-                    tableName: [
-                        {
-                            PutRequest: {
-                                Item: item
-                            }
-                        }
-                    ]
-                },
-            });
-            // If some questions fail to write, add them to a list of failures
-            if (UnprocessedItems.length) {
-                failedItems.push(UnprocessedItems);
-            }
-        }
+//         let requestItems = {};
+//         requestItems[tableName] = itemArray;
+//         const params = {
+//             RequestItems: requestItems
+//         };
+//         const { UnprocessedItems } = await dynamoDb.batchWrite(params);
+//         // Todo: process UnprocessedItems
+//         let failedItems = [];
+//         for (const item of items) {
+//             const { UnprocessedItems } = await dynamoDb.batchWrite({
+//                 RequestItems: {
+//                     tableName: [
+//                         {
+//                             PutRequest: {
+//                                 Item: item
+//                             }
+//                         }
+//                     ]
+//                 },
+//             });
+//             // If some questions fail to write, add them to a list of failures
+//             if (UnprocessedItems.length) {
+//                 failedItems.push(UnprocessedItems);
+//             }
+//         }
 
-        // // retry any failed entries
-        // if (failedItems.length) {
-        //     const { UnprocessedItems } = await dynamoDb.batchWrite(tableName, {
-        //         RequestItems: { [questionTableName]: failedItems },
-        //     });
+//         // // retry any failed entries
+//         // if (failedItems.length) {
+//         //     const { UnprocessedItems } = await dynamoDb.batchWrite(tableName, {
+//         //         RequestItems: { [questionTableName]: failedItems },
+//         //     });
 
-        //     // if some still fail, add them to a list of items to be returned, return status 500
-        //     if (UnprocessedItems.length) {
-        //         console.error(
-        //             `Failed to add all questions from template to question table `
-        //         );
-        //     }
-    }
-}
+//         //     // if some still fail, add them to a list of items to be returned, return status 500
+//         //     if (UnprocessedItems.length) {
+//         //         console.error(
+//         //             `Failed to add all questions from template to question table `
+//         //         );
+//         //     }
+//     }
+// };
 
 const getTableNames = () => {
     let tableNames = [];
@@ -88,15 +87,13 @@ const getTableNames = () => {
     tableNames.push(process.env.StatusTableName);
     tableNames.push(process.env.AuthUserTableName);
     return tableNames;
-}
+};
 export const main = handler(async (event, context) => {
     if (event.source === "serverless-plugin-warmup") return null;
 
-    const params = {};
-
     const tableNames = getTableNames();
     console.log('-------------------------------------------------------------------');
-     dynamoDb.listTables({},(l=>console.log(l)));
+    dynamoDb.listTables({}, l => console.log(l));
     // Todo: Check if all table has a trigger with postKafkaData as part of its name
 
     for (const tableName of tableNames) {
@@ -113,14 +110,14 @@ export const main = handler(async (event, context) => {
                     startingKey,
                     keepSearching,
                 );
-                const count = items.length;
-                console.log('item count: ', count, ' last item: ', JSON.stringify(items(count-1)));
+            const count = items.length;
+            console.log('item count: ', count, ' last item: ', JSON.stringify(items(count - 1)));
             // addLastSynced(items);
             // await batchWrite(tableName, items);
         }
     }
-    
-    const addLastSynced = items => {
 
-    }
+    // const addLastSynced = items => {
+
+    // }
 });
