d623c4c82640377a4bea9ecc9f42dcb30f6dcf4a
Hasan Farook <hasanfar@gmail.com>
2021-08-24 11:25:45 -0400

    better logging

--- services/app-api/handlers/kafka/get/forceKafkaSync.js
+++ services/app-api/handlers/kafka/get/forceKafkaSync.js
@@ -36,10 +36,10 @@ const mergeLastSynced =
     (items, syncDateTime) => items.map(item => ({ ...item, lastSynced: syncDateTime }));
 
 const batchWrite = async (tableName, items) => {
-    console.log(`Performing batchwrite for ${items.length}. Table: ${tableName}`);
+    console.log(`Performing batchwrite for ${items.length} items in table: ${tableName}`);
     // split items into chunks of 25
     const itemChunks = chunk(items, 25);
-    console.log(`Items split into ${itemChunks.length} chunks of 25 items`);
+    console.log(`Items split into ${itemChunks.length} chunk(s) of not more than 25 items`);
     for (const index in itemChunks) {
         // Construct the request params for batchWrite
         const itemArray = itemChunks[index].map(item => {
@@ -56,20 +56,20 @@ const batchWrite = async (tableName, items) => {
         const params = {
             RequestItems: requestItems
         };
-        console.log('Performing batchWrite...');
-        const { UnprocessedItems } = await dynamoDb.batchWrite(params);
 
-        console.log(`BatchWrite ran with ${UnprocessedItems.length ?? 0} numbers of failed record updates`);
-
-        if ((UnprocessedItems.length ?? 0) > 0) {
-            const keys = UnprocessedItems.map(item => item[Object.keys(item)[0]]);
-            console.log(`Unprocessed records for the table ${tableName} with keys ${keys}`);
+        const { FailedItems } = await dynamoDb.batchWrite(params);
+        console.log(`BatchWrite performed for ${(itemArray.length)} items`);
+        if ((FailedItems?.length ?? 0) > 0) {
+            const keys = FailedItems.map(item => item[Object.keys(item)[0]]);
+            console.log(`BatchWrite ran with ${FailedItems.length ?? 0} numbers of failed item updates`);
+            console.log(`The following items failed updating for the table ${tableName} -  keys ${keys}`);
         }
     }
 };
 
 export const main = handler(async (event, context) => {
     if (event.source === "serverless-plugin-warmup") return null;
+
     const syncDateTime = new Date().toISOString();
 
     for (const tableName of tableNames) {
@@ -79,8 +79,8 @@ export const main = handler(async (event, context) => {
         let keepSearching = true;
         // Looping to perform complete scan of tables due to 1 mb limit per iteration
         while (keepSearching) {
-
             let data;
+
             try {
                 [startingKey, keepSearching, data] =
                     await scanTable(
@@ -91,7 +91,8 @@ export const main = handler(async (event, context) => {
             }
             catch (err) {
                  console.error(`Database scan failed for the table ${tableName}
-                     with startingKey ${startingKey} and the keepSearching flag is ${keepSearching}`);
+                     with startingKey ${startingKey} and the keepSearching flag is ${keepSearching}.
+                     Error: ${err}`);
                 continue;
             }
 
