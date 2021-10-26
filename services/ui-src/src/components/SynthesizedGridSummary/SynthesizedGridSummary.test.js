e8853a06d29ff6cbefff83219d1a1e4d1c0a691f
Samuel Sierra <ssierra@collabralink.com>
2021-10-25 20:50:25 -0400

    rewrite tests

--- services/ui-src/src/components/SynthesizedGridSummary/SynthesizedGridSummary.test.js
+++ services/ui-src/src/components/SynthesizedGridSummary/SynthesizedGridSummary.test.js
@@ -8,27 +8,7 @@ import {
   mockGridData
 } from "../../provider-mocks/synthesizedGridSummaryMock";
 import configureStore from "redux-mock-store";
-import { Provider } from "react-redux";
-import currentFormMock_64_21E from "../../provider-mocks/currentFormMock_64_21E.js";
-const mockStore = configureStore([]);
-
 describe("Test SynthesizedGridSummary.js", () => {
-  let store;
-  let wrapper;
-
-  beforeEach(() => {
-    store = mockStore(currentFormMock_64_21E);
-    wrapper = mount(
-      <Provider store={store}>
-        <SynthesizedGridSummary
-          allAnswers={mockAllAnswers}
-          questionID={mockQuestionID}
-          gridData={mockGridData}
-          label={mockLabel}
-        />
-      </Provider>
-    );
-  });
 
   test("Rewrite", () => {
     expect(1).toBe(1);
