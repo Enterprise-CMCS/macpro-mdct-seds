692a640f335a7a2287fac09dd5f4f4d20313754e
Samuel Sierra <ssierra@collabralink.com>
2021-10-25 20:53:29 -0400

    rewrite tests

--- services/ui-src/src/components/SynthesizedGridSummary/SynthesizedGridSummary.test.js
+++ services/ui-src/src/components/SynthesizedGridSummary/SynthesizedGridSummary.test.js
@@ -1,16 +1,36 @@
-import React from "react";
-import { mount } from "enzyme";
-import SynthesizedGridSummary from "./SynthesizedGridSummary";
-import {
-  mockQuestionID,
-  mockLabel,
-  mockAllAnswers,
-  mockGridData
-} from "../../provider-mocks/synthesizedGridSummaryMock";
-import configureStore from "redux-mock-store";
-describe("Test SynthesizedGridSummary.js", () => {
+// import React from "react";
+// import { mount } from "enzyme";
+// import SynthesizedGridSummary from "./SynthesizedGridSummary";
+// import {
+//   mockQuestionID,
+//   mockLabel,
+//   mockAllAnswers,
+//   mockGridData
+// } from "../../provider-mocks/synthesizedGridSummaryMock";
+// import configureStore from "redux-mock-store";
+// import { Provider } from "react-redux";
+// import currentFormMock_64_21E from "../../provider-mocks/currentFormMock_64_21E.js";
+// const mockStore = configureStore([]);
 
-  test("Rewrite", () => {
-    expect(1).toBe(1);
-  });
-});
+// describe("Test SynthesizedGridSummary.js", () => {
+//   let store;
+//   let wrapper;
+
+//   beforeEach(() => {
+//     store = mockStore(currentFormMock_64_21E);
+//     wrapper = mount(
+//       <Provider store={store}>
+//         <SynthesizedGridSummary
+//           allAnswers={mockAllAnswers}
+//           questionID={mockQuestionID}
+//           gridData={mockGridData}
+//           label={mockLabel}
+//         />
+//       </Provider>
+//     );
+//   });
+
+//   test("Rewrite", () => {
+//     expect(1).toBe(1)
+//   });
+// });
