4a3a442b822dc4271009d3a0f5be1c895652df8d
Andrew Belzer <abelzer@collabralink.com>
2021-11-29 20:46:23 -0600

    import webpack

--- services/.sechub/handlers/sync.js
+++ services/.sechub/handlers/sync.js
@@ -1,3 +1,7 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+
 // Our data source is Security Hub Findings.  Auth is provided via the lambda's IAM role.
 import AWS from "aws-sdk";
 const securityhub = new AWS.SecurityHub();
