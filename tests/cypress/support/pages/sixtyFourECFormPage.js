7a56f6226c7eede748a19d9bc47c60732bb1b6ed
Guli <72164092+GL2020-QM@users.noreply.github.com>
2021-12-30 14:34:05 -0500

    added quarter1 6 forms page and step.js (#816)

---
+++ tests/cypress/support/pages/sixtyFourECFormPage.js
@@ -0,0 +1,102 @@
+const all_text_inputs = "//input[@data-testid='textInput']";
+const year2021 = "(//button[@class='usa-accordion__button'])[1]";
+const quarterOne = "(//ul[@class='quarterly-items']/li/a)[1]";
+const sixtyfourecFormLink = "(//a)[9]";
+const sixtyfourecFormTitle = "//h2";
+const saveButton = "//button[@class='usa-button']";
+const textArea = "//textarea[@id='summaryNotesInput']";
+const summaryLink = "//li[@id='react-tabs-10']";
+const age1_5Link = "//li[@id='react-tabs-2']";
+const age6_12Link = "//li[@id='react-tabs-4']";
+const age13_18Link = "//li[@id='react-tabs-6']";
+const age19_20Link = "//li[@id='react-tabs-8']";
+
+
+export class sixtyFourECFormPage {
+
+    open64ecform(){
+        cy.xpath(year2021).click();
+        cy.wait(2000);
+        cy.xpath(quarterOne).click();
+        cy.wait(2000);
+        cy.xpath(sixtyfourecFormLink).click();
+        
+    }
+
+    verify64ecTitle(){
+        cy.xpath(sixtyfourecFormTitle).should('be.visible');
+        cy.xpath(sixtyfourecFormTitle).contains('Number of Children Served in Medicaid Program');
+    }
+    
+  
+    verifyData1Inputs(){
+        cy.xpath(all_text_inputs).each((item, index, list) => {
+            //expect(list).to.have.length(27);      // number of element item
+            cy.wrap(item).scrollIntoView();
+            cy.wrap(item).clear();
+            cy.wait(100);
+            cy.wrap(item).type("10");
+            cy.wait(100);
+        });
+    }
+
+    verifyData2Inputs(){
+        cy.xpath(age1_5Link).click();
+        cy.xpath(all_text_inputs).each((item, index, list) => {
+            //expect(list).to.have.length(27);      // number of element item
+            cy.wrap(item).scrollIntoView();
+            cy.wrap(item).clear();
+            cy.wait(100);
+            cy.wrap(item).type("10");
+            cy.wait(100);
+        });
+    }
+
+    verifyData3Inputs(){
+        cy.xpath(age6_12Link).click();
+        cy.xpath(all_text_inputs).each((item, index, list) => {
+            //expect(list).to.have.length(27);      // number of element item
+            cy.wrap(item).scrollIntoView();
+            cy.wrap(item).clear();
+            cy.wait(100);
+            cy.wrap(item).type("10");
+            cy.wait(100);
+        });
+    }
+
+    verifyData4Inputs(){
+        cy.xpath(age13_18Link).click();
+        cy.xpath(all_text_inputs).each((item, index, list) => {
+            //expect(list).to.have.length(27);      // number of element item
+            cy.wrap(item).scrollIntoView();
+            cy.wrap(item).clear();
+            cy.wait(100);
+            cy.wrap(item).type("10");
+            cy.wait(100);
+        });
+    }
+
+    verifyData5Inputs(){
+        cy.xpath(age19_20Link).click();
+        cy.xpath(all_text_inputs).each((item, index, list) => {
+            //expect(list).to.have.length(27);      // number of element item
+            cy.wrap(item).scrollIntoView();
+            cy.wrap(item).clear();
+            cy.wait(100);
+            cy.wrap(item).type("10");
+            cy.wait(100);
+        });
+    }
+
+    enterTextForSummary(){
+        cy.xpath(summaryLink).click();
+        cy.xpath(textArea).clear();
+        cy.wait(1000);
+        cy.xpath(textArea).type('This is a test for Summary section');
+    }
+
+    clickSaveButton(){
+        cy.xpath(saveButton).click();
+    }
+  }
+  export default sixtyFourECFormPage;
