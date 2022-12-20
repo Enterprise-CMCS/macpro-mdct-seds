d248a4bd06699bf58460fa6e2f942a0c069eb0bf
Brendan Myers <6362494+BearHanded@users.noreply.github.com>
2022-12-20 13:02:41 -0500

    Try/Catch + Logs & Await saves (#1106)
    * Wrap everything in a try catch for better responsiveness
    
    * Await write to DB
    
    * error code
    
    * Fix status code for errors
    
    Co-authored-by: Nicholas Summers <nick@coforma.io>

--- services/app-api/handlers/state-forms/post/generateEnrollmentTotals.js
+++ services/app-api/handlers/state-forms/post/generateEnrollmentTotals.js
@@ -23,7 +23,7 @@ export const main = handler(async (event, context) => {
     return generatedTotals;
   }
 
-  const commitResponse = commitTotalsToDB(generatedTotals.countsToWrite);
+  const commitResponse = await commitTotalsToDB(generatedTotals.countsToWrite);
   if (commitResponse.status === 404) {
     return commitResponse;
   }
@@ -35,227 +35,253 @@ export const main = handler(async (event, context) => {
 });
 
 const getStateForms = async (forms) => {
-  // Build expression Attribute Value object
-  const getExpressions = () => {
-    const expressionAttributeValues = {
-      ":quarter": 4,
-    };
-    let filterExpression = "";
-    for (const i in forms) {
-      const key = `:form_${i}`;
-      expressionAttributeValues[key] = forms[i];
-      if (key === ":form_0") {
-        filterExpression += `(form = ${key} `;
-      } else {
-        filterExpression += `OR form = ${key}`;
+  try {
+    // Build expression Attribute Value object
+    const getExpressions = () => {
+      const expressionAttributeValues = {
+        ":quarter": 4,
+      };
+      let filterExpression = "";
+      for (const i in forms) {
+        const key = `:form_${i}`;
+        expressionAttributeValues[key] = forms[i];
+        if (key === ":form_0") {
+          filterExpression += `(form = ${key} `;
+        } else {
+          filterExpression += `OR form = ${key}`;
+        }
       }
+      filterExpression += ") AND quarter = :quarter";
+      return [expressionAttributeValues, filterExpression];
+    };
+
+    const [expressionAttributeValues, filterExpression] = getExpressions();
+
+    const params = {
+      TableName:
+        process.env.STATE_FORMS_TABLE_NAME ?? process.env.StateFormsTableName,
+      Select: "ALL_ATTRIBUTES",
+      ExpressionAttributeValues: { ...expressionAttributeValues },
+      FilterExpression: filterExpression,
+      ConsistentRead: true,
+    };
+
+    let startingKey;
+    let existingItems = [];
+    let results;
+
+    const scanTable = async (startingKey) => {
+      params.ExclusiveStartKey = startingKey;
+      let results = await dynamoDb.scan(params);
+      if (results.LastEvaluatedKey) {
+        startingKey = results.LastEvaluatedKey;
+        return [startingKey, results];
+      } else {
+        return [null, results];
+      }
+    };
+
+    // Looping to perform complete scan of tables due to 1 mb limit per iteration
+    do {
+      [startingKey, results] = await scanTable(startingKey);
+
+      const items = results.Items;
+      existingItems.push(...items);
+    } while (startingKey);
+
+    if (existingItems.length === 0) {
+      return {
+        status: 404,
+        message: "Could not find any matching state forms",
+      };
     }
-    filterExpression += ") AND quarter = :quarter";
-    return [expressionAttributeValues, filterExpression];
-  };
-
-  const [expressionAttributeValues, filterExpression] = getExpressions();
-
-  const params = {
-    TableName:
-      process.env.STATE_FORMS_TABLE_NAME ?? process.env.StateFormsTableName,
-    Select: "ALL_ATTRIBUTES",
-    ExpressionAttributeValues: { ...expressionAttributeValues },
-    FilterExpression: filterExpression,
-    ConsistentRead: true,
-  };
-
-  let startingKey;
-  let existingItems = [];
-  let results;
-
-  const scanTable = async (startingKey) => {
-    params.ExclusiveStartKey = startingKey;
-    let results = await dynamoDb.scan(params);
-    if (results.LastEvaluatedKey) {
-      startingKey = results.LastEvaluatedKey;
-      return [startingKey, results];
-    } else {
-      return [null, results];
-    }
-  };
-
-  // Looping to perform complete scan of tables due to 1 mb limit per iteration
-  do {
-    [startingKey, results] = await scanTable(startingKey);
-
-    const items = results.Items;
-    existingItems.push(...items);
-  } while (startingKey);
-
-  if (existingItems.length === 0) {
     return {
-      status: 404,
-      message: "Could not find any matching state forms",
+      status: 200,
+      message: `Get State Forms Called and Returned Forms Successfully`,
+      entries: existingItems,
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: "Something went wrong:\n" + error,
     };
   }
-  return {
-    status: 200,
-    message: `Get State Forms Called and Returned Forms Successfully`,
-    entries: existingItems,
-  };
 };
 
 const generateTotals = async (stateForms, ageRange) => {
-  const countsToWrite = [];
-  // Loop through all stateForms
-  let stateFormsLength = stateForms.length;
-  for (let i = 0; i < stateFormsLength; i++) {
-    const questionAccumulator = [];
-    let questionTotal = 0;
-    let ageRangeLength = ageRange.length;
-    for (let j = 0; j < ageRangeLength; j++) {
-      const answerEntry = `${stateForms[i].state_form}-${ageRange[j]}-07`;
+  try {
+    const countsToWrite = [];
+    // Loop through all stateForms
+    let stateFormsLength = stateForms.length;
+    for (let i = 0; i < stateFormsLength; i++) {
+      const questionAccumulator = [];
+      let questionTotal = 0;
+      let ageRangeLength = ageRange.length;
+      for (let j = 0; j < ageRangeLength; j++) {
+        const answerEntry = `${stateForms[i].state_form}-${ageRange[j]}-07`;
 
-      const questionParams = {
-        TableName:
-          process.env.FORM_ANSWERS_TABLE_NAME ??
-          process.env.FormAnswersTableName,
-        ExpressionAttributeValues: {
-          ":answerEntry": answerEntry,
-        },
-        KeyConditionExpression: "answer_entry = :answerEntry",
-      };
+        const questionParams = {
+          TableName:
+            process.env.FORM_ANSWERS_TABLE_NAME ??
+            process.env.FormAnswersTableName,
+          ExpressionAttributeValues: {
+            ":answerEntry": answerEntry,
+          },
+          KeyConditionExpression: "answer_entry = :answerEntry",
+        };
 
-      let startingKey;
-      let existingItems = [];
-      let results;
+        let startingKey;
+        let existingItems = [];
+        let results;
 
-      const queryTable = async (startingKey) => {
-        questionParams.ExclusiveStartKey = startingKey;
-        let results = await dynamoDb.query(questionParams);
-        if (results.LastEvaluatedKey) {
-          startingKey = results.LastEvaluatedKey;
-          return [startingKey, results];
-        } else {
-          return [null, results];
+        const queryTable = async (startingKey) => {
+          questionParams.ExclusiveStartKey = startingKey;
+          let results = await dynamoDb.query(questionParams);
+          if (results.LastEvaluatedKey) {
+            startingKey = results.LastEvaluatedKey;
+            return [startingKey, results];
+          } else {
+            return [null, results];
+          }
+        };
+
+        // Looping to perform complete scan of tables due to 1 mb limit per iteration
+        do {
+          [startingKey, results] = await queryTable(startingKey);
+
+          const items = results.Items;
+          existingItems.push(...items);
+        } while (startingKey);
+
+        // Add just the rows, no other details are needed
+        let questionResultLength = existingItems.length;
+        for (let k = 0; k < questionResultLength; k++) {
+          questionAccumulator.push(existingItems[k].rows);
         }
-      };
 
-      // Looping to perform complete scan of tables due to 1 mb limit per iteration
-      do {
-        [startingKey, results] = await queryTable(startingKey);
-
-        const items = results.Items;
-        existingItems.push(...items);
-      } while (startingKey);
-
-      // Add just the rows, no other details are needed
-      let questionResultLength = existingItems.length;
-      for (let k = 0; k < questionResultLength; k++) {
-        questionAccumulator.push(existingItems[k].rows);
+        // Calculate totals (add all columns together only if they are numbers)
+        questionTotal = totalEnrollmentCount(questionAccumulator);
       }
 
-      // Calculate totals (add all columns together only if they are numbers)
-      questionTotal = totalEnrollmentCount(questionAccumulator);
-    }
+      // Setup counts object
+      let countObject = [];
+      if (stateForms[i].form === "21E") {
+        countObject = {
+          type: "separate",
+          year: stateForms[i].year,
+          count: questionTotal,
+        };
+      }
 
-    // Setup counts object
-    let countObject = [];
-    if (stateForms[i].form === "21E") {
-      countObject = {
-        type: "separate",
-        year: stateForms[i].year,
-        count: questionTotal,
-      };
-    }
+      if (stateForms[i].form === "64.21E") {
+        countObject = {
+          type: "expansion",
+          year: stateForms[i].year,
+          count: questionTotal,
+        };
+      }
 
-    if (stateForms[i].form === "64.21E") {
-      countObject = {
-        type: "expansion",
-        year: stateForms[i].year,
-        count: questionTotal,
-      };
-    }
-
-    countsToWrite.push({
-      PutRequest: {
-        Item: {
-          ...stateForms[i],
-          enrollmentCounts: countObject,
+      countsToWrite.push({
+        PutRequest: {
+          Item: {
+            ...stateForms[i],
+            enrollmentCounts: countObject,
+          },
         },
-      },
-    });
+      });
 
-    if (countsToWrite.Count === 0) {
-      return {
-        status: 404,
-        message: "Could not retrieve Answer Entries",
-      };
+      if (countsToWrite.Count === 0) {
+        return {
+          status: 404,
+          message: "Could not retrieve Answer Entries",
+        };
+      }
     }
+    return {
+      status: 200,
+      message: "Answer Entries found and counts accumulated",
+      countsToWrite,
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: "Something went wrong:\n" + error,
+    };
   }
-  return {
-    status: 200,
-    message: "Answer Entries found and counts accumulated",
-    countsToWrite,
-  };
 };
 
 const commitTotalsToDB = async (putRequests) => {
-  const failureList = [];
-  const retryFailLimit = 5;
+  try {
+    const failureList = [];
+    const retryFailLimit = 5;
 
-  // Batch write all items, rerun if any UnprocessedItems are returned and it's under the retry limit
-  const batchWriteAll = async (tryRetryBatch) => {
-    // Attempt first batch write
-    const { UnprocessedItems } = await dynamoDb.batchWrite(tryRetryBatch.batch);
-
-    // If there are any failures and under the retry limit
-    if (UnprocessedItems.length && tryRetryBatch.noOfRetries < retryFailLimit) {
-      const retryBatch = {
-        noOfRetries: tryRetryBatch.noOfRetries + 1,
-        batch: UnprocessedItems,
-      };
-      return await batchWriteAll(retryBatch);
-    } else if (tryRetryBatch.noOfRetries >= retryFailLimit) {
-      // exceeded failure limit
-      console.error(
-        `Tried batch ${
-          tryRetryBatch.noOfRetries
-        } times. Failing batch ${JSON.stringify(tryRetryBatch)}`
+    // Batch write all items, rerun if any UnprocessedItems are returned and it's under the retry limit
+    const batchWriteAll = async (tryRetryBatch) => {
+      // Attempt first batch write
+      const { UnprocessedItems } = await dynamoDb.batchWrite(
+        tryRetryBatch.batch
       );
-      failureList.push({ failure: JSON.stringify(tryRetryBatch) });
-    }
-  };
 
-  // Begin batching by groups of 25
-  const batchArrayFormAnswers = [];
-  const batchSizeFA = 25;
-  for (let i = 0; i < putRequests.length; i += batchSizeFA) {
-    batchArrayFormAnswers.push(putRequests.slice(i, i + batchSizeFA));
-  }
-
-  // Get tableName
-  const formAnswersTableName =
-    process.env.STATE_FORMS_TABLE_NAME ?? process.env.StateFormsTableName;
-
-  // Loop through batches and write to DB
-  for (let i in batchArrayFormAnswers) {
-    const batchRequest = {
-      RequestItems: {
-        [formAnswersTableName]: batchArrayFormAnswers[i],
-      },
+      // If there are any failures and under the retry limit
+      if (
+        UnprocessedItems.length &&
+        tryRetryBatch.noOfRetries < retryFailLimit
+      ) {
+        const retryBatch = {
+          noOfRetries: tryRetryBatch.noOfRetries + 1,
+          batch: UnprocessedItems,
+        };
+        return await batchWriteAll(retryBatch);
+      } else if (tryRetryBatch.noOfRetries >= retryFailLimit) {
+        // exceeded failure limit
+        console.error(
+          `Tried batch ${
+            tryRetryBatch.noOfRetries
+          } times. Failing batch ${JSON.stringify(tryRetryBatch)}`
+        );
+        failureList.push({ failure: JSON.stringify(tryRetryBatch) });
+      }
     };
 
-    // Process this batch
-    await batchWriteAll({ batch: batchRequest, noOfRetries: 0 });
-  }
+    // Begin batching by groups of 25
+    const batchArrayFormAnswers = [];
+    const batchSizeFA = 25;
+    for (let i = 0; i < putRequests.length; i += batchSizeFA) {
+      batchArrayFormAnswers.push(putRequests.slice(i, i + batchSizeFA));
+    }
 
-  if (failureList.length > 0) {
+    // Get tableName
+    const formAnswersTableName =
+      process.env.STATE_FORMS_TABLE_NAME ?? process.env.StateFormsTableName;
+
+    // Loop through batches and write to DB
+    for (let i in batchArrayFormAnswers) {
+      const batchRequest = {
+        RequestItems: {
+          [formAnswersTableName]: batchArrayFormAnswers[i],
+        },
+      };
+
+      // Process this batch
+      await batchWriteAll({ batch: batchRequest, noOfRetries: 0 });
+    }
+
+    if (failureList.length > 0) {
+      return {
+        status: 500,
+        message: `Failed to write all entries to database.`,
+      };
+    }
+    return {
+      status: 200,
+      message: `Totals Updated Successfully`,
+    };
+  } catch (error) {
     return {
       status: 500,
-      message: `Failed to write all entries to database.`,
+      message: "Something went wrong:\n" + error,
     };
   }
-  return {
-    status: 200,
-    message: `Totals Updated Successfully`,
-  };
 };
 
 const totalEnrollmentCount = (questionAccumulator) => {
