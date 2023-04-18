3a8dc57fdc414ac177fc18c19e2997dd4f16897b
benmartin-coforma <Ben.Martin@coforma.io>
2023-04-18 15:53:59 -0600

    Adjust the date math in generateQuarterForms.js

--- services/app-api/handlers/forms/post/generateQuarterForms.js
+++ services/app-api/handlers/forms/post/generateQuarterForms.js
@@ -111,7 +111,6 @@ export const main = handler(async (event, context) => {
   };
 
   // Get year and quarter from request, or the current date for automated jobs
-  const currentDate = new Date();
   let specifiedYear;
   let specifiedQuarter;
 
@@ -124,15 +123,10 @@ export const main = handler(async (event, context) => {
     }
   }
 
-  // If no year, use current year sent in data object
-  if (!specifiedYear) {
-    specifiedYear = getSpecifiedYear(currentDate);
-  }
-
-  // If no quarter provided in data object, determine what quarter it is
-  if (!specifiedQuarter) {
-    specifiedQuarter = getQuarter(currentDate);
-  }
+  // If not specified, determine the reporting period from the current date.
+  const currentQuarter = calculateFormQuarterFromDate(new Date());
+  specifiedYear = specifiedYear || currentQuarter.year;
+  specifiedQuarter = specifiedQuarter || currentQuarter.quarter;
 
   // Search for existing stateForms
   const foundForms = await findExistingStateForms(
@@ -354,3 +348,29 @@ export const main = handler(async (event, context) => {
     message: `Forms successfully created for Quarter ${specifiedQuarter} of ${specifiedYear}`,
   };
 });
+
+/**
+ * We run an automated process at the start of each quarter,
+ * which generates forms for the previous quarter.
+ * For example: on Jan 1 2024, we would generate forms for Oct-Dec 2023.
+ * Those forms would be due for completion by Jan 31, 2024.
+ * 
+ * A potential source of confusion is that Oct-Dec 2023 represents the
+ * federal fiscal quarter 2024 Q1; a quarter ahead of what you may expect.
+ * Another potential source is that we want to generate forms for the
+ * _previous_ quarter, to report on data from the recent past.
+ * 
+ * Happily, these off-by-one issues cancel each other out. So this
+ * function returns the more common quarter number of the current date.
+ * 
+ * @param { Date } date - The current date
+ * @returns { Object } { year, quarter } -
+ *          The Federal Fiscal Quarter for which forms should be generated
+ */
+export const calculateFormQuarterFromDate = (date) => {
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let quarter = month % 3 + 1
+
+  return { year, quarter };
+}
