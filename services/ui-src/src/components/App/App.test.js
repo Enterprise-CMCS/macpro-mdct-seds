5397d458121b29ee7ac1e614c334ad8b76db6e9d
brittbujacich <britt.bujacich@gmail.com>
2023-08-02 14:35:26 -0600

    fix test

--- services/ui-src/src/components/App/App.test.js
+++ services/ui-src/src/components/App/App.test.js
@@ -2,6 +2,11 @@ import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
 
+// mocking config file because the redirect affects this test setup
+jest.mock("../../config/config", () => {
+  return {};
+});
+
 describe("Test App.js", () => {
   test("Check the main div, with classname app, exists", () => {
     const wrapper = shallow(<App />);
