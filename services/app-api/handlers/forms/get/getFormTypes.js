a4cd75c25ed0710c9b11236e096a78e2ace42e5c
karla-vm <karla@coforma.io>
2024-01-19 14:08:44 -0500

    missing import

--- services/app-api/handlers/forms/get/getFormTypes.js
+++ services/app-api/handlers/forms/get/getFormTypes.js
@@ -1,5 +1,6 @@
 import handler from "../../../libs/handler-lib";
 import dynamoDb from "../../../libs/dynamodb-lib";
+import { getUserCredentialsFromJwt } from "../../../libs/authorization";
 
 export const main = handler(async (event, context) => {
   // If this invokation is a prewarm, do nothing and return.
