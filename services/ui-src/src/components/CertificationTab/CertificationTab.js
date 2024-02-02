468f4bee39d8f74eb9c699d2b4b0ae93327eef5c
karla-vm <karla@coforma.io>
2024-02-02 13:38:30 -0500

    temporarily disable the email to state users + bo users flow

--- services/ui-src/src/components/CertificationTab/CertificationTab.js
+++ services/ui-src/src/components/CertificationTab/CertificationTab.js
@@ -61,7 +61,12 @@ const CertificationTab = ({
     if (window.confirm("Are you sure you want to uncertify this report?")) {
       await uncertify();
       saveForm();
-      await sendEmailtoBo();
+      /* 
+        NOTE: The SEDS business owners have requested that the email flow to users be disabled, but would like to be
+        able to re-enable it at a future point (see: https://bit.ly/3w3mVmT). For now, this call will be commented out
+        and not removed.
+      */
+      // await sendEmailtoBo();
       setprovisionalButtonStatus(false);
       setfinalButtonStatus(false);
     }
