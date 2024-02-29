cbbf38bb7126d7ae1fb76a86869c699ccdc645fe
karla-vm <karla@coforma.io>
2024-02-29 10:11:12 -0500

    linting

--- services/ui-src/src/components/CertificationTab/CertificationTab.js
+++ services/ui-src/src/components/CertificationTab/CertificationTab.js
@@ -83,17 +83,20 @@ const CertificationTab = ({
     return userRole;
   };
 
-  // NOTE: The SEDS business owners have requested that the email flow to users be disabled, but would like to be
-  // able to re-enable it at a future point (see: https://bit.ly/3w3mVmT). For now, this will be commented out and not removed.
-  // const sendEmailtoBo = async () => {
-  //   let userRole = await currentUserRole();
-  //   if (userRole === "state") {
-  //     let emailObj = {
-  //       formInfo: formStatus
-  //     };
-  //     await sendUncertifyEmail(emailObj);
-  //   }
-  // };
+  /* 
+    NOTE: The SEDS business owners have requested that the email flow to users be disabled, but would like to be
+    able to re-enable it at a future point (see: https://bit.ly/3w3mVmT). For now, this will be commented out and not removed.
+    
+    const sendEmailtoBo = async () => {
+      let userRole = await currentUserRole();
+      if (userRole === "state") {
+              let emailObj = {
+        formInfo: formStatus
+      };
+      await sendUncertifyEmail(emailObj);
+    }
+  };
+  */
 
   let certifyText;
 
