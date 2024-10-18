f1ea9848fb58b98f7fbe6cda9aa2298234cbce0c
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-10-18 13:17:40 -0700

    Update bundling-config.ts

--- lib/config/bundling-config.ts
+++ lib/config/bundling-config.ts
@@ -3,7 +3,4 @@ import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
 export const commonBundlingOptions: lambda.BundlingOptions = {
   minify: true,
   sourceMap: true,
-  define: {
-    __IS_FRONTEND__: "false",
-  },
 };
