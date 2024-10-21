af20328a7f9e1341bfae049982eba6cdb7ef2492
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-10-21 05:33:14 -0700

    Update ui-src.ts

--- lib/stacks/ui-src.ts
+++ lib/stacks/ui-src.ts
@@ -1,16 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
-interface StackProps extends cdk.NestedStackProps {
+interface UiSrcStackProps extends cdk.NestedStackProps {
   stack: string;
 }
 
 export class UiSrcStack extends cdk.NestedStack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: UiSrcStackProps) {
     super(scope, id, props);
 
-    const stage = this.node.tryGetContext("stage") || "dev";
+    // const stage = this.node.tryGetContext("stage") || "dev";
 
-    console.log("TODO: implement this stack", stage);
+    // Define S3 bucket for UI hosting
+    // const uiBucket =
+    new s3.Bucket(this, "UiBucket", {
+      // bucketName: props.s3BucketName,
+      // websiteIndexDocument: "index.html",
+      // websiteErrorDocument: "index.html",
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false,
+    });
+
   }
 }
