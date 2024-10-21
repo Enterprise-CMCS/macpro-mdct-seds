2aa1807e28a105ea96844d0972089475cd2aff98
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-10-21 09:19:14 -0700

    Update index.ts

--- lib/stacks/index.ts
+++ lib/stacks/index.ts
@@ -3,6 +3,4 @@ export * from "./data";
 export * from "./stream-functions";
 export * from "./ui-auth";
 export * from "./ui-src";
-export * from "./ui-waf-log-assoc";
-export * from "./ui-waflog-s3-bucket";
 export * from "./ui";
