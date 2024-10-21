5495c528d1a3db47a559d14a9089c8d8e74c6cb1
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-10-21 16:10:29 -0700

    Update ui-auth.ts

--- lib/stacks/ui-auth.ts
+++ lib/stacks/ui-auth.ts
@@ -1,16 +1,40 @@
 import * as cdk from "aws-cdk-lib";
+import {
+  aws_iam as iam,
+} from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-interface StackProps extends cdk.NestedStackProps {
+interface UiAuthStackProps extends cdk.NestedStackProps {
   stack: string;
 }
 
 export class UiAuthStack extends cdk.NestedStack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: UiAuthStackProps) {
     super(scope, id, props);
 
-    const stage = this.node.tryGetContext("stage") || "dev";
 
-    console.log("TODO: implement this stack", stage);
+
+    // IAM Role for Lambda
+    const lambdaApiRole = new iam.Role(this, "LambdaApiRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSLambdaVPCAccessExecutionRole"
+        ),
+      ],
+    });
+
+    lambdaApiRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: ["arn:aws:logs:*:*:*"],
+      })
+    );
+
   }
 }
