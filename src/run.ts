a05e0453e37d22b9b2757135d0459140b8ea7440
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-11-05 11:00:14 -0800

    Update run.ts

--- src/run.ts
+++ src/run.ts
@@ -129,18 +129,32 @@ async function run_api_locally(runner: LabeledProcessRunner) {
     ["yarn", "install"],
     "services/app-api"
   );
+<<<<<<< HEAD
+=======
+
+  await runner.run_command_and_output(
+    "api synth",
+    [
+      "cdk",
+      "synth",
+      // "--no-staging" // TODO: determine if this is helpful
+    ],
+    "services/app-api"
+  );
+
+>>>>>>> a390a945 (Update run.ts)
   runner.run_command_and_output(
     "api",
     [
-      "serverless",
-      "offline",
-      "start",
-      "--stage",
+      "sam",
       "local",
-      "--region",
-      "us-east-1",
-      "--httpPort",
+      "start-api",
+      // "--template", // TODO: determine if this is helpful
+      // "./cdk.out/AppApiStack.template.json", // TODO: determine if this is helpful
+      "--port",
       "3030",
+      // "--warm-containers", // TODO: determine if this is helpful
+      // "EAGER", // TODO: determine if this is helpful
     ],
     "services/app-api"
   );
@@ -154,6 +168,8 @@ async function run_fe_locally(runner: LabeledProcessRunner) {
     ["yarn", "install"],
     "services/ui-src"
   );
+
+  // TODO: update this to match my changes to env.sh
   await runner.run_command_and_output(
     "ui conf",
     ["./env.sh", "local"],
