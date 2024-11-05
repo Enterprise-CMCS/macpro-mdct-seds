d0ca5b3640ebb4f2e4a4156f48a9c7b3e64b4ad3
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-11-05 10:57:45 -0800

    update run.ts from using `serverless dynamodb` to use the java lib directly

--- src/run.ts
+++ src/run.ts
@@ -93,8 +93,16 @@ async function run_db_locally(runner: LabeledProcessRunner) {
   );
   runner.run_command_and_output(
     "db",
-    ["serverless", "dynamodb", "start", "--stage=local"],
-    "services/database"
+    [
+      "java",
+      "-Djava.library.path=./DynamoDBLocal_lib",
+      "-jar",
+      "DynamoDBLocal.jar",
+      "-sharedDb",
+      "-port",
+      "8000",
+    ],
+    "services/database/dynamodb-local"
   );
   await new Promise((res) => setTimeout(res, 8 * 1000)); // The above runners need to all finish, not all can be awaited, they block
   await runner.run_command_and_output(
