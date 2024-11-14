6ad5f564a92d91992586d2ef3fd20aac4d47ebe6
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-11-13 18:23:28 -0800

    add logging to getting systems manager parameter store parameter

--- lib/utils/ssm.ts
+++ lib/utils/ssm.ts
@@ -1,13 +1,14 @@
-import {
-  SSMClient,
-  GetParameterCommand,
-} from "@aws-sdk/client-ssm";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
 // If the param is a SecureString, decrypt it. No effect otherwise.
 const WithDecryption = true;
 const client = new SSMClient({ region: "us-east-1" });
 
 export async function getParameter(parameterName: string) {
+  console.log(
+    "getting systems manager parameter store parameter:",
+    parameterName
+  );
   const command = new GetParameterCommand({
     Name: parameterName,
     WithDecryption,
