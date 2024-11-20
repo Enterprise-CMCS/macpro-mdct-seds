213283bf3d92d9dedc6e1c6ad9f7cd6f78fb54cd
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2024-11-20 14:07:40 -0800

    Update cloudwatch-logs-resource-policy.ts

--- deployment/constructs/cloudwatch-logs-resource-policy.ts
+++ deployment/constructs/cloudwatch-logs-resource-policy.ts
@@ -1,6 +1,6 @@
 import { Construct } from "constructs";
 import { CfnResourcePolicy } from "aws-cdk-lib/aws-logs";
-import { Stack } from "aws-cdk-lib";
+import { Stack, aws_iam as iam } from "aws-cdk-lib";
 
 interface CloudWatchLogsResourcePolicyProps {
   readonly project: string;
@@ -12,37 +12,41 @@ export class CloudWatchLogsResourcePolicy extends Construct {
   constructor(
     scope: Construct,
     id: string,
-    props: CloudWatchLogsResourcePolicyProps,
+    props: CloudWatchLogsResourcePolicyProps
   ) {
     super(scope, id);
     const stack = Stack.of(this);
+
+    const policyDocument = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          principals: [new iam.ServicePrincipal("delivery.logs.amazonaws.com")],
+          actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+          resources: [
+            `arn:aws:logs:*:${stack.account}:log-group:aws-waf-logs-*`,
+            `arn:aws:logs:*:${stack.account}:log-group:/aws/http-api/*`,
+            `arn:aws:logs:*:${stack.account}:log-group:/aws/vendedlogs/*`,
+          ],
+          conditions: {
+            StringEquals: { "aws:SourceAccount": stack.account },
+            ArnLike: {
+              "aws:SourceArn": `arn:aws:logs:${stack.region}:${stack.account}:*`,
+            },
+          },
+        }),
+      ],
+    });
+
+    policyDocument.validateForResourcePolicy();
+
     this.policy = new CfnResourcePolicy(
       this,
       `CentralizedCloudWatchLogsResourcePolicy`,
       {
         policyName: `${props.project}-centralized-logs-policy-${id}`,
-        policyDocument: JSON.stringify({
-          Version: "2012-10-17",
-          Statement: [
-            {
-              Effect: "Allow",
-              Principal: { Service: "delivery.logs.amazonaws.com" },
-              Action: ["logs:CreateLogStream", "logs:PutLogEvents"],
-              Resource: [
-                `arn:aws:logs:*:${stack.account}:log-group:aws-waf-logs-*`,
-                `arn:aws:logs:*:${stack.account}:log-group:/aws/http-api/*`,
-                `arn:aws:logs:*:${stack.account}:log-group:/aws/vendedlogs/*`,
-              ],
-              Condition: {
-                StringEquals: { "aws:SourceAccount": stack.account },
-                ArnLike: {
-                  "aws:SourceArn": `arn:aws:logs:${stack.region}:${stack.account}:*`,
-                },
-              },
-            },
-          ],
-        }),
-      },
+        policyDocument: policyDocument.toJSON(),
+      }
     );
   }
 }
