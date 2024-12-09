00b3fcdcbc3fd591bf5ce95dea1efc005118d3fb
Pete Dunlap <pete.dunlap@stratiform.digital>
2024-12-09 11:27:44 -0500

    CMDCT-4184 removing some things to prep for import

--- src/run.ts
+++ src/run.ts
@@ -46,7 +46,7 @@ async function confirmDestroyCommand(stack: string) {
 
   const confirmation = await question(`
 ${orange}********************************* STOP *******************************
-You've requested a destroy for: 
+You've requested a destroy for:
 
     ${stack}
 
@@ -130,6 +130,8 @@ async function deploy(options: { stage: string }) {
   const stage = options.stage;
   const runner = new LabeledProcessRunner();
   await prepare_services(runner);
+  // const importCmd = ["cdk", "import", "--context", `stage=${stage}`]; //#--force
+  // await runner.run_command_and_output("CDK import", importCmd, ".");
   const deployCmd = ["cdk", "deploy", "--context", `stage=${stage}`, "--all"];
   await runner.run_command_and_output("CDK deploy", deployCmd, ".");
 }
