4b6132804030228ca81d108e6c0b181530224253
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2024-12-18 11:30:31 -0700

    Replace react-data-table-component (#15004)

--- services/ui-src/src/components/Users/Users.test.jsx
+++ services/ui-src/src/components/Users/Users.test.jsx
@@ -79,12 +79,10 @@ describe("Test Users.js", () => {
       "PDF",
       { selector: "button" }
     )).toBeInTheDocument();
-
-    expect(screen.getByPlaceholderText("Filter Table")).toBeInTheDocument();
   });
 
   it("should render the correct headers", async () => {
-    const { container } = renderComponent();
+    renderComponent();
     await waitFor(() => expect(listUsers).toHaveBeenCalled());
 
     const expectedHeaders = [
@@ -97,7 +95,7 @@ describe("Test Users.js", () => {
       "Last Login",
       "States",
     ]
-    const headers = [...container.querySelectorAll(".rdt_TableHeadRow > div")];
+    const headers = screen.getAllByRole("columnheader");
     expect(headers.length).toBe(expectedHeaders.length);
     for (let i = 0; i < expectedHeaders.length; i += 1) {
       expect(headers[i].textContent).toBe(expectedHeaders[i]);
@@ -108,7 +106,7 @@ describe("Test Users.js", () => {
     const { container } = renderComponent();
     await waitFor(() => expect(listUsers).toHaveBeenCalled());
 
-    const rows = [...container.querySelectorAll(".rdt_TableBody [role='row']")];
+    const rows = [...container.querySelectorAll("table tbody tr")];
     expect(rows.length).toBe(mockUsers.length);
 
     let row1cells = rows[0].childNodes;
