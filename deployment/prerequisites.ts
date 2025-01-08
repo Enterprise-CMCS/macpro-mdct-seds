3c1174bce370c96191632c8d2936b4c9ffbd2b52
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-01-08 13:16:56 -0500

    CMDCT-4226 - sets up deploy-prerequisites to create resources needed in AWS account (#15025)
    Co-authored-by: Jon Holman <9025884+JonHolman@users.noreply.github.com>

---
+++ deployment/prerequisites.ts
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import {
+  aws_apigateway as apigateway,
+  aws_iam as iam,
+  App,
+  DefaultStackSynthesizer,
+  Stack,
+  StackProps,
+  Tags,
+} from "aws-cdk-lib";
+import { CloudWatchLogsResourcePolicy } from "./constructs/cloudwatch-logs-resource-policy";
+import { loadDefaultSecret } from "./deployment-config";
+import { getSecret } from "./utils/secrets-manager";
+import { Construct } from "constructs";
+
+interface PrerequisiteConfigProps {
+  project: string;
+  iamPath: string;
+  iamPermissionsBoundaryArn: string;
+}
+
+export class PrerequisiteStack extends Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: StackProps & PrerequisiteConfigProps
+  ) {
+    super(scope, id, props);
+
+    const {
+      project,
+      iamPermissionsBoundaryArn,
+      iamPath,
+    } = props;
+
+    new CloudWatchLogsResourcePolicy(this, "logPolicy", { project });
+
+    const cloudWatchRole = new iam.Role(
+      this,
+      "ApiGatewayRestApiCloudWatchRole",
+      {
+        assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+        permissionsBoundary: iam.ManagedPolicy.fromManagedPolicyArn(
+          this,
+          "iamPermissionsBoundary",
+          iamPermissionsBoundaryArn
+        ),
+        path: iamPath,
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+          ),
+        ],
+      }
+    );
+
+    new apigateway.CfnAccount(
+      this,
+      "ApiGatewayRestApiAccount",
+      {
+        cloudWatchRoleArn: cloudWatchRole.roleArn,
+      }
+    );
+  }
+}
+
+async function main() {
+  const app = new App({
+    defaultStackSynthesizer: new DefaultStackSynthesizer(
+      JSON.parse((await getSecret("cdkSynthesizerConfig"))!)
+    ),
+  });
+
+  Tags.of(app).add("PROJECT", "SEDS");
+
+  const project = process.env.PROJECT!;
+  new PrerequisiteStack(app, "seds-prerequisites", {
+    project,
+    ...(await loadDefaultSecret(project)),
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  });
+}
+
+main();
