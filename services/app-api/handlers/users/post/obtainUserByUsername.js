b67ca4b9895faeec4965c9756330ff41e480eb15
benmartin-coforma <Ben.Martin@coforma.io>
2025-02-03 16:47:21 -0700

    Add missing awaits

--- services/app-api/handlers/users/post/obtainUserByUsername.js
+++ services/app-api/handlers/users/post/obtainUserByUsername.js
@@ -9,7 +9,7 @@ export const main = handler(async (event, context) => {
   await authorizeAnyUser(event);
   let data = JSON.parse(event.body);
   const result = await obtainUserByUsername(data.username);
-  authorizeAdminOrUserWithEmail(result.Items[0].email);
+  await authorizeAdminOrUserWithEmail(result.Items[0].email);
   return result;
 });
 
