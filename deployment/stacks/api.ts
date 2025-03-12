027e5c4a265d6a02207962dcbfd920acabd4dd9b
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-03-12 15:37:35 -0600

    revert changes to deployment/stacks/api.ts

--- deployment/stacks/api.ts
+++ deployment/stacks/api.ts
@@ -52,8 +52,7 @@ export function createApiComponents(props: CreateApiComponentsProps) {
   Tags.of(scope).add("SERVICE", service);
 
   const vpc = ec2.Vpc.fromLookup(scope, "Vpc", { vpcName });
-  const kafkaAuthorizedSubnets =
-    getSubnets(scope, kafkaAuthorizedSubnetIds) || vpc.privateSubnets;
+  const kafkaAuthorizedSubnets = getSubnets(scope, kafkaAuthorizedSubnetIds)
 
   const kafkaSecurityGroup = new ec2.SecurityGroup(
     scope,
