3da660993ea029fd5c5cbc7c13a0c6b9b0ce6eb8
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-09-10 19:32:26 -0400

    updates

--- cli/lib/utils.ts
+++ cli/lib/utils.ts
@@ -7,46 +7,62 @@ import { runCommand } from "../lib/runner";
 import { region } from "./consts";
 
 export const getCloudFormationStackOutputValues = async (
-  stackName: string,
-  outputNames: string[]
-): Promise<{ [key: string]: string }> => {
-  const cloudFormationClient = new CloudFormationClient({ region });
+  stackName: string
+): Promise<Record<string, string>> => {
+  const cloudFormationClient = new CloudFormationClient({
+    region,
+  });
   const command = new DescribeStacksCommand({ StackName: stackName });
   const response = await cloudFormationClient.send(command);
 
-  const outputs = response.Stacks?.[0]?.Outputs!;
-  const result: { [key: string]: string } = {};
+  const outputs = response.Stacks?.[0]?.Outputs ?? [];
+  return Object.fromEntries(
+    outputs
+      .map(
+        (o) => [o.OutputKey ?? (o as any).OutputName, o.OutputValue] as const
+      )
+      .filter(([k]) => Boolean(k)) as [string, string | undefined][]
+  );
+};
 
-  for (const outputName of outputNames) {
-    result[outputName] = outputs.find(
-      (output) => output.OutputKey === outputName
-    )?.OutputValue!;
+export const buildUiEnvObject = (
+  stage: string,
+  cfnOutputs: Record<string, string | undefined>
+): Record<string, string> => {
+  if (stage === "localstack") {
+    return {
+      SKIP_PREFLIGHT_CHECK: "true",
+      API_REGION: region,
+      API_URL: cfnOutputs.ApiUrl.replace("https", "http"),
+      COGNITO_REGION: region,
+      COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID!,
+      COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID!,
+      COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID!,
+      COGNITO_USER_POOL_CLIENT_DOMAIN:
+        process.env.COGNITO_USER_POOL_CLIENT_DOMAIN!,
+      COGNITO_REDIRECT_SIGNIN: "http://localhost:3000/",
+      COGNITO_REDIRECT_SIGNOUT: "http://localhost:3000/",
+    };
   }
 
-  return result;
+  return {
+    SKIP_PREFLIGHT_CHECK: "true",
+    API_REGION: region,
+    API_URL: cfnOutputs.ApiUrl,
+    COGNITO_REGION: region,
+    COGNITO_IDENTITY_POOL_ID: cfnOutputs.CognitoIdentityPoolId,
+    COGNITO_USER_POOL_ID: cfnOutputs.CognitoUserPoolId,
+    COGNITO_USER_POOL_CLIENT_ID: cfnOutputs.CognitoUserPoolClientId,
+    COGNITO_USER_POOL_CLIENT_DOMAIN: `${cfnOutputs.CognitoUserPoolClientDomain}.auth.${region}.amazoncognito.com`,
+    COGNITO_REDIRECT_SIGNIN: cfnOutputs.CloudFrontUrl,
+    COGNITO_REDIRECT_SIGNOUT: cfnOutputs.CloudFrontUrl,
+  };
 };
 
 export const runFrontendLocally = async (stage: string) => {
-  if (stage === "localstack") {
-    const { ApiUrl } = await getCloudFormationStackOutputValues(
-      `seds-${stage}`,
-      ["ApiUrl"]
-    );
-    await writeLocalUiEnvFile({ ApiUrl });
-  } else {
-    const outputValues = await getCloudFormationStackOutputValues(
-      `seds-${stage}`,
-      [
-        "ApiUrl",
-        "CognitoIdentityPoolId",
-        "CognitoUserPoolId",
-        "CognitoUserPoolClientId",
-        "CognitoUserPoolClientDomain",
-      ]
-    );
-
-    await writeLocalUiEnvFile(outputValues);
-  }
+  const outputs = await getCloudFormationStackOutputValues(`seds-${stage}`);
+  const envVars = buildUiEnvObject(stage, outputs);
+  await writeLocalUiEnvFile(envVars);
 
   runCommand("ui", ["yarn", "start"], "services/ui-src");
 };
--- cli/lib/write-ui-env-file.ts
+++ cli/lib/write-ui-env-file.ts
@@ -1,7 +1,6 @@
 import path, { dirname } from "path";
 import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
-import { region } from "./consts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,46 +8,9 @@ const configFilePath = path.resolve(
   path.join(__dirname, "../../services/ui-src/public/env-config.js")
 );
 
-export const writeLocalUiEnvFile = async (values: {
-  [key: string]: string;
-}) => {
-  const apiUrl = values["ApiUrl"];
-  if (!apiUrl) throw new Error("ApiUrl is required");
-
-  const hasFullOutputs = [
-    "CognitoIdentityPoolId",
-    "CognitoUserPoolId",
-    "CognitoUserPoolClientId",
-    "CognitoUserPoolClientDomain",
-  ].every((k) => Boolean(values[k]));
-
-  const envVariables = hasFullOutputs
-    ? {
-        SKIP_PREFLIGHT_CHECK: "true",
-        API_REGION: region,
-        API_URL: values["ApiUrl"],
-        COGNITO_REGION: region,
-        COGNITO_IDENTITY_POOL_ID: values["CognitoIdentityPoolId"],
-        COGNITO_USER_POOL_ID: values["CognitoUserPoolId"],
-        COGNITO_USER_POOL_CLIENT_ID: values["CognitoUserPoolClientId"],
-        COGNITO_USER_POOL_CLIENT_DOMAIN: `${values["CognitoUserPoolClientDomain"]}.auth.${region}.amazoncognito.com`,
-        COGNITO_REDIRECT_SIGNIN: "http://localhost:3000/",
-        COGNITO_REDIRECT_SIGNOUT: "http://localhost:3000/",
-      }
-    : {
-        SKIP_PREFLIGHT_CHECK: "true",
-        API_REGION: region,
-        API_URL: apiUrl.replace("https", "http"),
-        COGNITO_REGION: region,
-        COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
-        COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-        COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID,
-        COGNITO_USER_POOL_CLIENT_DOMAIN:
-          process.env.COGNITO_USER_POOL_CLIENT_DOMAIN,
-        COGNITO_REDIRECT_SIGNIN: "http://localhost:3000/",
-        COGNITO_REDIRECT_SIGNOUT: "http://localhost:3000/",
-      };
-
+export const writeLocalUiEnvFile = async (
+  envVariables: Record<string, string>
+) => {
   await fs.rm(configFilePath, { force: true });
 
   const envConfigContent = [
