432c760d28aa31835659fba1771344dcea75c6c0
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2025-09-19 16:01:33 -0500

    fix structure

--- services/ui-src/src/components/Footer/Footer.jsx
+++ services/ui-src/src/components/Footer/Footer.jsx
@@ -7,47 +7,47 @@ const Footer = () => {
     <div className="footerRoot" data-testid="Footer" role="contentinfo">
       <div className="footerTop">
         <GridContainer className="footerTopContainer" containerSize="none">
-          <Grid col={6} tablet={{ col: true }} className="footerTopFlex">
-            <div className="footerTopLeftContainer">
+          <Grid row className="footerTopFlex">
+            <Grid col={6} tablet={{ col: true }} className="footerTopLeftContainer">
               <img
                 className="sedsLogo"
                 src="/img/seds-logo.svg"
                 alt="MDCT SEDS: Statistical Enrollment Data Systems, Medicaid Data Collection Tool"
               />
-            </div>
-          </Grid>
-          <Grid col={6} tablet={{ col: true }}  className="footerTopRightContainer">
-            <div className="footerTopRightTopFlex">
-              <div className="footerCMSBrandingLeft">
-                <div className="hhsLogo">
-                  <img
-                      src="/img/logo_hhs.svg"
-                      alt="Department of Health and Human Services, USA"
-                  />
+            </Grid>
+            <Grid col={6} tablet={{ col: true }} className="footerTopRightContainer">
+              <div className="footerTopRightTopFlex">
+                <div className="footerCMSBrandingLeft">
+                  <div className="hhsLogo">
+                    <img
+                        src="/img/logo_hhs.svg"
+                        alt="Department of Health and Human Services, USA"
+                    />
+                  </div>
+                  <div className="hhsMedicaidLogoMobile">
+                    <img
+                        src="/img/logo_medicaid.svg"
+                        alt="Medicaid.gov: Keeping America Healthy"
+                    />
+                  </div>
                 </div>
-                <div className="hhsMedicaidLogoMobile">
+                <div className="footerCMSBrandingRight">
+                  <p className="hhsCopyText">
+                    A federal government website managed and paid for by the
+                    U.S. Centers for Medicare and Medicaid Services and part of
+                    the MDCT suite.
+                  </p>
+                </div>
+              </div>
+              <div className="footerCMSMedicaid">
+                <div className="medicaidLogo">
                   <img
                       src="/img/logo_medicaid.svg"
                       alt="Medicaid.gov: Keeping America Healthy"
                   />
                 </div>
               </div>
-              <div className="footerCMSBrandingRight">
-                <p className="hhsCopyText">
-                  A federal government website managed and paid for by the
-                  U.S. Centers for Medicare and Medicaid Services and part of
-                  the MDCT suite.
-                </p>
-              </div>
-            </div>
-            <div className="footerCMSMedicaid">
-              <div className="medicaidLogo">
-                <img
-                    src="/img/logo_medicaid.svg"
-                    alt="Medicaid.gov: Keeping America Healthy"
-                />
-              </div>
-            </div>
+            </Grid>
           </Grid>
         </GridContainer>
       </div>
