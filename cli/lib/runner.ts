d9c7fd9ab634c19c4e7625888b933e248ed051a0
Source Files <action@github.com>
2025-09-24 21:43:17 +0000

    sync: update source files

--- cli/lib/runner.ts
+++ cli/lib/runner.ts
@@ -1,18 +1,31 @@
-/* eslint-disable multiline-comment-style */
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 import { spawn } from "child_process";
 import path from "path";
 
 const prefixes = new Set<string>();
 let maxPrefixLength = 0;
 
+const prefixColors: Record<string, string> = {};
+// prettier-ignore
+const colors = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"];
+
 const formattedPrefix = (prefix: string) => {
   if (!prefixes.has(prefix)) {
     prefixes.add(prefix);
+
     if (prefix.length > maxPrefixLength) {
       maxPrefixLength = prefix.length;
     }
+
+    const color = colors.shift();
+    if (color) {
+      colors.push(color);
+      prefixColors[prefix] = color;
+    }
   }
-  return ` ${prefix.padStart(maxPrefixLength)}|`;
+
+  const color = prefixColors[prefix];
+  return `\x1b[38;5;${color}m ${prefix.padStart(maxPrefixLength)}|\x1b[0m`;
 };
 
 export const runCommand = async (
--- cli/lib/sts.ts
+++ cli/lib/sts.ts
@@ -1,5 +1,6 @@
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
-import { region } from "./consts";
+import { region } from "./consts.js";
 
 export const checkIfAuthenticated = async (): Promise<void> => {
   const client = new STSClient({ region });
