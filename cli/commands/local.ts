e6210cc4be7f7e2120d4fae6959d0bf568240e5b
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-10-28 21:58:04 -0400

    a

--- cli/commands/local.ts
+++ cli/commands/local.ts
@@ -48,11 +48,12 @@ export const local = {
     process.env.AWS_ENDPOINT_URL = "http://localhost.localstack.cloud:4566";
     process.env.AWS_ENDPOINT_URL_S3 = "http://s3.localhost.localstack.cloud:4566";
 
+    const cdklocalBin = "deployment/node_modules/.bin/cdklocal";
+
     await runCommand(
       "CDK local bootstrap",
       [
-        "bun",
-        "cdklocal",
+        cdklocalBin,
         "bootstrap",
         `aws://000000000000/${region}`, // LocalStack uses the default dummy account ID 000000000000
         "--context",
@@ -64,8 +65,7 @@ export const local = {
     await runCommand(
       "CDK local local-prerequisite deploy",
       [
-        "bun",
-        "cdklocal",
+        cdklocalBin,
         "deploy",
         "--app",
         "bun tsx deployment/local/prerequisites.ts",
@@ -76,8 +76,7 @@ export const local = {
     await runCommand(
       "CDK local prerequisite deploy",
       [
-        "bun",
-        "cdklocal",
+        cdklocalBin,
         "deploy",
         "--app",
         "bun tsx deployment/prerequisites.ts",
@@ -90,8 +89,7 @@ export const local = {
     await runCommand(
       "CDK local deploy",
       [
-        "bun",
-        "cdklocal",
+        cdklocalBin,
         "deploy",
         "--context",
         "stage=localstack",
@@ -107,8 +105,7 @@ export const local = {
       runCommand(
         "CDK local watch",
         [
-          "bun",
-          "cdklocal",
+          cdklocalBin,
           "watch",
           "--context",
           "stage=localstack",
