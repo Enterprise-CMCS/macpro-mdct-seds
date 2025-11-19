df19d6cadce39d36a60387ae37e45f350165bbf2
BearHanded <bcm3223@gmail.com>
2025-11-19 13:26:45 -0500

    correct redirect config in amplify configure

--- services/ui-src/src/index.jsx
+++ services/ui-src/src/index.jsx
@@ -17,8 +17,8 @@ Amplify.configure({
       loginWith: {
         oauth: {
           domain: config.cognito.APP_CLIENT_DOMAIN,
-          redirectSignIn: config.cognito.REDIRECT_SIGNIN,
-          redirectSignOut: config.cognito.REDIRECT_SIGNOUT,
+          redirectSignIn: [config.cognito.REDIRECT_SIGNIN],
+          redirectSignOut: [config.cognito.REDIRECT_SIGNOUT],
           scope: ["email", "openid"],
           responseType: "token"
         }
