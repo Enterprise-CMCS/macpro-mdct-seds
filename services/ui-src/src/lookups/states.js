40e3993e13c6416b51184e7a9726ff69b121c081
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2025-11-25 13:25:41 -0700

    Remove US states from Redux

---
+++ services/ui-src/src/lookups/states.js
@@ -0,0 +1,68 @@
+export const states = {
+  AK: "Alaska",
+  AL: "Alabama",
+  AR: "Arkansas",
+  AZ: "Arizona",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DC: "District Of Columbia",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  IA: "Iowa",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  MA: "Massachusetts",
+  MD: "Maryland",
+  ME: "Maine",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MO: "Missouri",
+  MS: "Mississippi",
+  MT: "Montana",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  NE: "Nebraska",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NV: "Nevada",
+  NY: "New York",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VA: "Virginia",
+  VT: "Vermont",
+  WA: "Washington",
+  WI: "Wisconsin",
+  WV: "West Virginia",
+  WY: "Wyoming",
+};
+
+/**
+ * Given a U.S. state abbreviation, what is that state's full name?
+ * @param {string} abbr
+ * @example
+ * getStateName("CO") //=> "Colorado"
+ */
+export const getStateName = (abbr) => states[abbr];
+
+/**
+ * An array of `{ label, value }` objects suitable for a dropdown menu.
+ */
+export const stateSelectOptions = Object.entries(states)
+  .map(([abbr, name]) => ({ label: name, value: abbr }))
+  .sort((a, b) => a.label.localeCompare(b.label));
--- services/ui-src/src/store/reducers/global.js
+++ services/ui-src/src/store/reducers/global.js
@@ -1,23 +1,14 @@
 // Temporary import, using forms.json static data
 import * as age_ranges from "../to-delete/age_ranges.json";
-import * as states from "../to-delete/states.json";
 
 // ACTION TYPES
-export const LOAD_FORM_TYPES = "LOAD_FORM_TYPES";
 export const LOAD_AGE_RANGES = "LOAD_AGE_RANGES";
-export const LOAD_STATES = "LOAD_STATES";
 
 // ACTION CREATORS
 export const gotAgeRanges = (agesArray = []) => {
   return {
     type: LOAD_AGE_RANGES,
     agesArray
-  };
-};
-export const gotStates = (statesArray = []) => {
-  return {
-    type: LOAD_STATES,
-    statesArray
   };
 };
 
@@ -29,18 +20,9 @@ export const getAgeRanges = () => {
   };
 };
 
-export const getStates = () => {
-  return async dispatch => {
-    // Call aws amplify endpoint. This is a placeholder
-    // const data = fetchedData
-    // dispatch(gotStates(data));
-  };
-};
-
 // INITIAL STATE
 const initialState = {
-  age_ranges: [...age_ranges.default],
-  states: [...states.default]
+  age_ranges: [...age_ranges.default]
 };
 
 // REDUCER
@@ -50,11 +32,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         age_ranges: action.agesArray
-      };
-    case LOAD_STATES:
-      return {
-        ...state,
-        states: action.statesArray
       };
     default:
       return state;
