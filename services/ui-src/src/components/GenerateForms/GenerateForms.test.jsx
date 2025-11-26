8698991a68f0ca18a073adc468256343e8c602a9
ailZhou <aileen.zhou@coforma.io>
2025-11-26 16:03:35 -0500

    Update GenerateForms.test.jsx

--- services/ui-src/src/components/GenerateForms/GenerateForms.test.jsx
+++ services/ui-src/src/components/GenerateForms/GenerateForms.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { beforeEach, describe, expect, it, test, vi } from "vitest";
 import GenerateForms from "./GenerateForms";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { generateQuarterlyForms } from "../../libs/api";
 
@@ -9,50 +9,91 @@ vi.spyOn(window, "alert").mockImplementation(() => {});
 vi.spyOn(window, "confirm").mockImplementation(() => true);
 
 vi.mock("../../libs/api", () => ({
-  generateQuarterlyForms: vi.fn(),
+  generateQuarterlyForms: vi.fn()
 }));
 
 describe("Test GenerateForms.js", () => {
   let container;
   beforeEach(() => {
-    container = render(<GenerateForms/>).container;
+    container = render(<GenerateForms />).container;
     vi.clearAllMocks();
   });
 
   it("should render", () => {
     // A dropdown for year, and a dropdown for quarter
     expect(container.querySelectorAll(".Dropdown-root")).toHaveLength(2);
-    expect(screen.getByText("Generate Forms", { selector: "button" })).toBeInTheDocument();
+    expect(
+      screen.getByText("Generate Forms", { selector: "button" })
+    ).toBeInTheDocument();
   });
 
   test("Generating Forms should error if the user has not selected a year and quarter", () => {
     expect(window.alert).not.toHaveBeenCalled();
 
-    const generateButton = screen.getByText("Generate Forms", { selector: "button" });
+    const generateButton = screen.getByText("Generate Forms", {
+      selector: "button"
+    });
     userEvent.click(generateButton);
 
     expect(window.alert).toBeCalledWith("Please select a Year and Quarter");
   });
 
-  test("Generating Forms should send a request to the API", () => {
-    expect(generateQuarterlyForms).not.toHaveBeenCalled();
+  describe("Generating Forms should send a request to the API", async () => {
+    beforeEach(() => {
+      expect(generateQuarterlyForms).not.toHaveBeenCalled();
 
-    const yearDropdown = screen.getByText("Select a Year");
-    userEvent.click(yearDropdown);
-    const yearOption = screen.getByText("2022");
-    userEvent.click(yearOption);
+      const yearDropdown = screen.getByText("Select a Year");
+      userEvent.click(yearDropdown);
+      const yearOption = screen.getByText("2022");
+      userEvent.click(yearOption);
 
-    const quarterDropdown = screen.getByText("Select a Quarter");
-    userEvent.click(quarterDropdown);
-    const quarterOption = screen.getByText("Q2");
-    userEvent.click(quarterOption);
+      const quarterDropdown = screen.getByText("Select a Quarter");
+      userEvent.click(quarterDropdown);
+      const quarterOption = screen.getByText("Q2");
+      userEvent.click(quarterOption);
+    });
 
-    const generateButton = screen.getByText("Generate Forms", { selector: "button" });
-    userEvent.click(generateButton);
+    test("Generating Forms is succesful", async () => {
+      generateQuarterlyForms.mockReturnValue({
+        year: 2022,
+        quarter: 2,
+        status: 200,
+        message: "success"
+      });
+      const generateButton = screen.getByText("Generate Forms", {
+        selector: "button"
+      });
+      userEvent.click(generateButton);
+      await waitFor(() => {
+        expect(screen.getByText("success")).toBeInTheDocument();
+      });
+    });
 
-    expect(generateQuarterlyForms).toHaveBeenCalledWith({
-      year: 2022,
-      quarter: 2,
+    test("Generating Form has a warning", async () => {
+      generateQuarterlyForms.mockReturnValue({
+        status: 204,
+        message: "warning"
+      });
+      const generateButton = screen.getByText("Generate Forms", {
+        selector: "button"
+      });
+      userEvent.click(generateButton);
+      await waitFor(() => {
+        expect(screen.getByText("warning")).toBeInTheDocument();
+      });
+    });
+    test("Generating Form has an error", async () => {
+      generateQuarterlyForms.mockReturnValue({
+        status: 500,
+        message: "error"
+      });
+      const generateButton = screen.getByText("Generate Forms", {
+        selector: "button"
+      });
+      userEvent.click(generateButton);
+      await waitFor(() => {
+        expect(screen.getByText("error")).toBeInTheDocument();
+      });
     });
   });
 });
