bbf9234c58cc0d5ade34d2350bdf15c995e4b7b2
ailZhou <aileen.zhou@coforma.io>
2025-11-26 11:17:15 -0500

    Update Header.test.jsx

--- services/ui-src/src/components/Header/Header.test.jsx
+++ services/ui-src/src/components/Header/Header.test.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import Header from "./Header";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { signOut } from "aws-amplify/auth";
 
 let realUseContext;
 let useContextMock;
 
 vi.mock("config/config", () => ({
-  default: { cognito: {
-    REDIRECT_SIGNOUT: "elsewhere.com",
-  },
-}}))
+  default: {
+    cognito: {
+      REDIRECT_SIGNOUT: "elsewhere.com"
+    }
+  }
+}));
 
+vi.mock("aws-amplify/auth", () => ({
+  fetchAuthSession: vi
+    .fn()
+    .mockReturnValue({ tokens: { accessToken: "12345" } }),
+  signOut: vi.fn()
+  // signOut: vi.fn().mockImplementation(() => {
+  //   return { then: () => {} };
+  // })
+}));
 
 // *** set up mocks
 beforeEach(() => {
@@ -26,17 +39,23 @@ afterEach(() => {
 });
 
 describe("Test Header.js", () => {
-  test("Check if Header, exists", () => {
+  beforeEach(() => {
     useContextMock.mockReturnValue(true);
-
     const mockUser = { attributes: { "app-role": "admin" } };
-
-    const { getByTestId } = render(
+    render(
       <BrowserRouter>
         <Header user={mockUser} />
       </BrowserRouter>
     );
-
-    expect(getByTestId("Header")).toBeVisible();
+  });
+  test("Check if Header, exists", () => {
+    expect(screen.getByTestId("Header")).toBeVisible();
+  });
+  test("Test logout", async () => {
+    signOut.mockResolvedValueOnce();
+    await waitFor(() => expect(screen.getByText("My Profile")).toBeVisible());
+    const logoutBtn = screen.getByRole("button", { name: "Logout" });
+    userEvent.click(logoutBtn);
+    expect(signOut).toHaveBeenCalled();
   });
 });
