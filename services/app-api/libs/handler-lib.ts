be45019eb14ba464fd21e701f3024e9d42deabfd
Sophie Kwak <sophie.kwak@coforma.io>
2026-05-19 16:07:02 -0700

    simplified the code

--- services/app-api/libs/handler-lib.ts
+++ services/app-api/libs/handler-lib.ts
@@ -5,10 +5,7 @@ import {
   ParameterParser,
 } from "../shared/types.ts";
 import { AuthUser, scanForUserWithSub } from "../storage/users.ts";
-import {
-  UnauthenticatedError,
-  getUserDetailsFromEvent,
-} from "./authorization.ts";
+import { getUserDetailsFromEvent } from "./authorization.ts";
 import * as logger from "./debug-lib.ts";
 import {
   badRequest,
@@ -46,10 +43,6 @@ export default function handler<TParams>(
 
       return await lambda({ user, body, parameters });
     } catch (error) {
-      if (error instanceof UnauthenticatedError) {
-        return unauthenticated((error as Error).message);
-      }
-
       logger.error("Error: %O", error);
       return internalServerError((error as Error).message);
     } finally {
@@ -62,11 +55,16 @@ export default function handler<TParams>(
  * Read the cognito token. If appropriate, search the AuthUser DB table.
  */
 const determineUser = async (event: APIGatewayProxyEvent) => {
-  const userFromToken = getUserDetailsFromEvent(event);
-  if (event.path === "/determineCurrentUser") {
-    // determineCurrentUser creates AuthUser records, so they needn't already exist.
-    return userFromToken as AuthUser;
-  } else {
-    return await scanForUserWithSub(userFromToken.usernameSub);
+  try {
+    const userFromToken = getUserDetailsFromEvent(event);
+    if (event.path === "/determineCurrentUser") {
+      // determineCurrentUser creates AuthUser records, so they needn't already exist.
+      return userFromToken as AuthUser;
+    } else {
+      return await scanForUserWithSub(userFromToken.usernameSub);
+    }
+  } catch {
+    // Auth parsing failed - return 401
+    return null;
   }
 };
