9cb752319485c6a658096ea276d4e180e1ad7e3a
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2026-07-15 20:50:36 -0700

    normalize ministack emulator flag

--- deployment/local/util.ts
+++ deployment/local/util.ts
@@ -1,2 +1,3 @@
 // This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 export const isMiniStack = process.env.CDK_DEFAULT_ACCOUNT === "000000000000";
+export const isLocalAwsEmulator = isMiniStack;
